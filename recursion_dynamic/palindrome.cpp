047fedb7d10acebd7d752531790c7d4b72687e90
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2016-06-06 11:36:35 -0500

    palindrome recursion

---
+++ recursion_dynamic/palindrome.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool palindrome(string temp, int l, int r, int size)
+{
+	if (l >= r)
+		return true;
+
+	if (temp[l] != temp[r])
+		return false;
+	else
+		return palindrome(temp, l+1, r-1, size);
+}
+
+int main()
+{
+	string temp = "ppsddspp";
+	cout << palindrome(temp, 0, temp.size() - 1, temp.size()) << endl;
+
+	return 0;
+}
