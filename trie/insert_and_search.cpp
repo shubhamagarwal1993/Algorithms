6515fba25ae71a1fd751197b9e62a65da7da3f06
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2016-06-18 00:24:37 -0500

    insert search on trie

---
+++ trie/insert_and_search.cpp
@@ -0,0 +1,107 @@
+//Trie - efficient information retrieval data structure
+//Search for a key using Trie - O(M)		-M is maximum string length
+//Search for a key using BST  - O(M logN)	-M is maximum string length, N is number of keys in tree.
+
+//For Trie
+//Insert - O(M)								-M is key length
+//Search - O(M)								-M is key length
+//Memory - O(ALPHABET_SIZE * M * N)			-M is key length, N is number of keys in Trie
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+#define ALPHABET_SIZE 26
+
+//tree node
+struct TrieNode
+{
+	std::map<char, TrieNode*> mymap;
+	bool isLeaf;
+};
+
+//constructor for new node
+TrieNode* newTrieNode()
+{
+	TrieNode* trieNode = new TrieNode;
+	trieNode->mymap.clear();
+	trieNode->isLeaf = false;
+	return trieNode;
+}
+
+// If not present, inserts key into trie
+// If the key is prefix of trie node, just marks leaf node
+void insert(TrieNode *root, string key)
+{
+	for (int i = 0; i < key.length(); i++)
+	{
+		char index = key[i];
+		std::map<char, TrieNode*>::iterator it;
+		it = root->mymap.find(index);
+
+		if (it != root->mymap.end())
+		{
+			//found char, move on to next char
+			root = root->mymap[index];
+		}
+		else	// new character
+		{
+			TrieNode* temp = newTrieNode();
+			root->mymap[index] = temp;
+			root = root->mymap[index];
+		}
+	}
+
+	// mark last node as leaf
+	root->isLeaf = true;
+}
+
+// Returns true if key presents in trie, else false
+bool search(TrieNode *root, string key)
+{
+	for (int i = 0; i < key.length(); i++)
+	{
+		char index = key[i];
+		std::map<char, TrieNode*>::iterator it;
+		it = root->mymap.find(index);
+
+		if (it != root->mymap.end())
+		{
+			root = root->mymap[index];
+		}
+		else
+		{
+			cout << "word not found: " << key << endl;
+			return false;
+		}
+	}
+	cout << "word found: " << key << endl;
+}
+
+int main()
+{
+	TrieNode* root = newTrieNode();				//will be the root
+
+	string keys[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
+	int keys_size = sizeof(keys)/sizeof(keys[0]);
+
+	// Construct trie
+	for (int i = 0; i < keys_size; i++)
+		insert(root, keys[i]);
+
+	cout << "------" << endl;
+	search(root, "the");
+	search(root, "these");
+	search(root, "their");
+	search(root, "thaw");
+	cout << "------" << endl;
+
+	return 0;
+}
