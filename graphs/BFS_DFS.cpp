951fffa605e943f421ed2aa4b45205c1f63235ed
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-02-04 10:50:54 -0600

    github indentation and comment

--- graphs/BFS_DFS.cpp
+++ graphs/BFS_DFS.cpp
@@ -5,127 +5,127 @@ using namespace std;
 
 // This class represents a directed graph using adjacency list representation
 class Graph {
-	// No. of vertices
-	int V;
-	// Pointer to an array containing adjacency lists
-	list<int> *adj;
-	// A function used by DFS
-	void DFSUtil(int v, bool visited[]);
+    // No. of vertices
+    int V;
+    // Pointer to an array containing adjacency lists
+    list<int> *adj;
+    // A function used by DFS
+    void DFSUtil(int v, bool visited[]);
 
-	public:
-		// Constructor
-		Graph(int V);
+    public:
+        // Constructor
+        Graph(int V);
 
-		// function to add an edge to graph
-		void addEdge(int v, int w);
+        // function to add an edge to graph
+        void addEdge(int v, int w);
 
-		// prints BFS traversal from a given source s
-		void BFS(int s);
+        // prints BFS traversal from a given source s
+        void BFS(int s);
 
-		// prints DFS traversal of the complete graph
-		void DFS();
+        // prints DFS traversal of the complete graph
+        void DFS();
 };
 
 Graph::Graph(int V) {
-	this->V = V;
-	adj = new list<int>[V];
+    this->V = V;
+    adj = new list<int>[V];
 }
 
 void Graph::addEdge(int v, int w) {
-	// Add w to v’s list
-	adj[v].push_back(w);
+    // Add w to v’s list
+    adj[v].push_back(w);
 }
 
 Graph construct_graph() {
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
-	return g;
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+    return g;
 }
 
 void Graph::BFS(int s) {
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for(int i = 0; i < V; i++) {
-		visited[i] = false;
-	}
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++) {
+        visited[i] = false;
+    }
 
-	// Create a queue for BFS
-	list<int> queue;
+    // Create a queue for BFS
+    list<int> queue;
 
-	// Mark the current node as visited and enqueue it
-	visited[s] = true;
-	queue.push_back(s);
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
 
-	// 'i' will be used to get all adjacent vertices of a vertex
-	list<int>::iterator i;
+    // 'i' will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
 
-	while(!queue.empty()) {
-		// Dequeue a vertex from queue and print it
-		s = queue.front();
-		cout << s << " ";
-		queue.pop_front();
+    while(!queue.empty()) {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
 
-		// Get all adjacent vertices of the dequeued vertex s
-		// If a adjacent has not been visited, then mark it visited
-		// and enqueue it
-		for(i = adj[s].begin(); i != adj[s].end(); ++i) {
-			if(!visited[*i]) {
-				visited[*i] = true;
-				queue.push_back(*i);
-			}
-		}
-	}
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[s].begin(); i != adj[s].end(); ++i) {
+            if(!visited[*i]) {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
 }
 
 void Graph::DFSUtil(int v, bool visited[]) {
-	// Mark the current node as visited and print it
-	visited[v] = true;
-	cout << v << " ";
+    // Mark the current node as visited and print it
+    visited[v] = true;
+    cout << v << " ";
 
-	// Recur for all the vertices adjacent to this vertex
-	list<int>::iterator i;
-	for(i = adj[v].begin(); i != adj[v].end(); ++i) {
-		if(!visited[*i]) {
-			DFSUtil(*i, visited);
-		}
-	}
+    // Recur for all the vertices adjacent to this vertex
+    list<int>::iterator i;
+    for(i = adj[v].begin(); i != adj[v].end(); ++i) {
+        if(!visited[*i]) {
+            DFSUtil(*i, visited);
+        }
+    }
 }
 
 // The function to do DFS traversal. It uses recursive DFSUtil()
 void Graph::DFS() {
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for (int i = 0; i < V; i++) {
-		visited[i] = false;
-	}
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++) {
+        visited[i] = false;
+    }
 
-	// Call the recursive helper function to print DFS traversal
-	// starting from all vertices one by one
-	for (int i = 0; i < V; i++) {
-		if (visited[i] == false) {
-			DFSUtil(i, visited);
-		}
-	}
+    // Call the recursive helper function to print DFS traversal
+    // starting from all vertices one by one
+    for (int i = 0; i < V; i++) {
+        if (visited[i] == false) {
+            DFSUtil(i, visited);
+        }
+    }
 }
 
 int main() {
-	// construct graph
-	//   ___   0 - 1
-	//   \ /	\ /
-	//    3 ---- 2
+    // construct graph
+    //   ___   0 - 1
+    //   \ /    \ /
+    //    3 ---- 2
 
-	Graph g = construct_graph();
+    Graph g = construct_graph();
 
-	cout << "Breadth First Traversal" << "(starting from vertex 2): ";
-	g.BFS(2);
-	cout << endl;
-	cout << "Depth First Traversal: ";
-	g.DFS();
-	cout << endl;
-	return 0;
+    cout << "Breadth First Traversal" << "(starting from vertex 2): ";
+    g.BFS(2);
+    cout << endl;
+    cout << "Depth First Traversal: ";
+    g.DFS();
+    cout << endl;
+    return 0;
 }
--- heaps/median_of_integer_stream.cpp
+++ heaps/median_of_integer_stream.cpp
@@ -1,3 +1,9 @@
+// We will get the median of a running stream of integers
+// For total of odd numbers - return the middle position integer
+// For total of even numbers - return the avg of the 2 middle position integers
+//
+// We will solve by maintaining a max heap for the left side of the integers of the median, and a min heap for the right side
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -13,83 +19,77 @@ using namespace std;
 
 int get_median(priority_queue<int> max_heap, priority_queue<int, vector<int>, std::greater<int> > min_heap)
 {
-	if(max_heap.size() == min_heap.size() + 1)
-		return max_heap.top();
-	else if (max_heap.size() == min_heap.size())
-		return ((max_heap.top() + min_heap.top())/2);
-	else
-	{
-		cout << "error in heaps" << endl;
-		return -1;
-	}
+    if(max_heap.size() == min_heap.size() + 1)
+        return max_heap.top();
+    else if (max_heap.size() == min_heap.size())
+        return ((max_heap.top() + min_heap.top())/2);
+    else
+    {
+        cout << "error in heaps" << endl;
+        return -1;
+    }
 }
 
-void insert(priority_queue<int> &max_heap, priority_queue<int, vector<int>, std::greater<int> > &min_heap, int temp)
-{
-	if (max_heap.size() == min_heap.size() + 1)
-	{
-		cout << "second" << endl;
-		if(temp < max_heap.top())
-		{
-			max_heap.push(temp);				//max heap has 2 more elements than min heap
-			temp = max_heap.top();				//we get the highest value in max heap
-			max_heap.pop();						//temp has lowerest value for min heap and max heap is 1 more in size
-			min_heap.push(temp);				//max and min heap have same size
-		}
-		else
-			min_heap.push(temp);
-	}
-	else if (max_heap.size() == min_heap.size())
-	{
-		if (max_heap.empty())
-		{
-			max_heap.push(temp);
-		}
-		else if (min_heap.empty())
-		{
-			//we have to insert here after checking with max heap
-			if(temp < max_heap.top())
-			{
-				max_heap.push(temp);				//max heap has 2 more elements than min heap
-				temp = max_heap.top();				//we get the highest value in max heap
-				max_heap.pop();						//temp has lowerest value for min heap and max heap is 1 more in size
-				min_heap.push(temp);				//max and min heap have same size
-			}
-			else
-				min_heap.push(temp);
-		}
-		else if(temp > min_heap.top())				//since same size, max heap will get element
-		{
-			min_heap.push(temp);
-			temp = min_heap.top();
-			min_heap.pop();
-			max_heap.push(temp);				//max heap has one extra element now
-		}
-		else
-			max_heap.push(temp);
-	}
-	else
-		cout << "error in heaps" << endl;
+void insert(priority_queue<int> &max_heap, priority_queue<int, vector<int>, std::greater<int> > &min_heap, int temp) {
 
-	if (!min_heap.empty())
-		cout << "size " << min_heap.size() << " min " << min_heap.top() << endl;
-	else
-		cout << "min size " << min_heap.size() << endl;
-	cout << "max size " << max_heap.size() << " max " << max_heap.top() << endl;
-	cout << "median " << get_median(max_heap, min_heap) << endl;
+    // If max_heap > min_heap then insert new element in min_heap
+    if (max_heap.size() == min_heap.size() + 1) {
+        cout << "insert into min_heap" << endl;
+        if(temp < max_heap.top()) {
+            max_heap.push(temp);        // max_heap has 2 more elements than min_heap
+            temp = max_heap.top();      // get the highest value in max heap
+            max_heap.pop();             // temp has lowerest value for min_heap and max_heap is 1 more in size
+            min_heap.push(temp);        // max_heap and min_heap have same size now with corrent ordering of elements
+        }
+        else {
+            min_heap.push(temp);
+        }
+    } else if (max_heap.size() == min_heap.size()) {
+        if (max_heap.empty()) {
+            max_heap.push(temp);
+        } else if (min_heap.empty()) {
+            //we have to insert here after checking with max heap
+            if(temp < max_heap.top()) {
+                max_heap.push(temp);                //max heap has 2 more elements than min heap
+                temp = max_heap.top();                //we get the highest value in max heap
+                max_heap.pop();                        //temp has lowerest value for min heap and max heap is 1 more in size
+                min_heap.push(temp);                //max and min heap have same size
+            }
+            else {
+                min_heap.push(temp);
+            }
+        } else if(temp > min_heap.top()) {               //since same size, max heap will get element {
+            min_heap.push(temp);
+            temp = min_heap.top();
+            min_heap.pop();
+            max_heap.push(temp);                //max heap has one extra element now
+        } else {
+            max_heap.push(temp);
+        }
+    } else {
+        cout << "error in heaps" << endl;
+    }
+
+    if (!min_heap.empty())
+        cout << "size " << min_heap.size() << " min " << min_heap.top() << endl;
+    else
+        cout << "min size " << min_heap.size() << endl;
+
+    cout << "max size " << max_heap.size() << " max " << max_heap.top() << endl;
+    cout << "median " << get_median(max_heap, min_heap) << endl;
 }
 
-int main()
-{
-	int temp;
-	priority_queue<int> max_heap;
-	priority_queue<int, vector<int>, std::greater<int> > min_heap;
+int main() {
+    int input;
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, std::greater<int> > min_heap;
 
-	while(1)
-	{
-		cin >> temp;
-		insert(max_heap, min_heap, temp);
-	}
+    while(1) {
+        cin >> input;
+        insert(max_heap, min_heap, input);
+    }
 
-	return 0;
+    cout << "Closing program...!" << endl;
+    return 0;
 }
+
