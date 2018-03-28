e88ae742d04e6631546b617159e206ef4670242f
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-03-27 23:30:20 -0500

    O(n logn) soln for LIS

--- recursion_dynamic/longest_increasing_subsequence.cpp
+++ recursion_dynamic/longest_increasing_subsequence.cpp
@@ -43,13 +43,68 @@ int LIS(int arr[], int arr_size) {
     return max;
 }
 
+// binary search
+int ceilIndex(vector<int> &vec, int l, int r, int val) {
+    while(l < r-1) {
+        int mid = (r + l) / 2;
+        if(vec[mid] >= val) {
+            r = mid;
+        } else {
+            l = mid;
+        }
+    }
+
+    // send the greatest value before current value
+    return r;
+}
+
+int LIS_vector(vector<int> &vec) {
+    if(vec.size() == 0) return 0;
+
+    // make a vector of size number of elements and initialize all to 0
+    // make first element as first element of lis
+    vector<int> tail(vec.size(), 0);
+    tail[0] = vec[0];
+
+    // keep track of what length is filled up to now
+    int length = 1;
+
+    for(int i = 1; i < vec.size(); i++) {
+        // smaller than smallest element yet
+        if(vec[i] < tail[0]) {
+            tail[0] = vec[i];
+        // larger than largest element yet - make new list (clone) and add current element
+        } else if(vec[i] > tail[length-1]) {
+            tail[length++] = vec[i];
+        // find new index to insert current element
+        } else {
+            // we choose -1 for left index to avoid infinite while loop in binary search
+            int index = ceilIndex(tail, -1, length-1, vec[i]);
+            tail[index] = vec[i];
+            // since length does not change, a
+        }
+    }
+
+    for(int i = 0; i < tail.size(); i++) {
+        cout << tail[i] << " ";
+    }
+    cout << endl;
+
+    return length;
+}
+
 int main() {
 
-    int arr[] = {3, 10, 2, 1, 20};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-
     // Time complexity O(n^2)
+    //int arr[] = {3, 10, 2, 1, 20};
+    int arr[] = {2, 5, 3, 7, 11, 8, 10, 13, 6};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
     cout << LIS(arr, arr_size) << endl;
+
+    // Time complexity O(n logn)
+    vector<int> vec;
+    vec.assign(arr, arr+arr_size);
+    cout << LIS_vector(vec) << endl;
 
     return 0;
 }
