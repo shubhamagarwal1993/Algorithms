f20457904e150c7e1cb5300adf7529cdd4bf051e
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-04-30 01:27:54 -0500

    fixed indentation of reverse linked list

--- linked_list/reverse_linked_list.cpp
+++ linked_list/reverse_linked_list.cpp
@@ -9,75 +9,68 @@
 using namespace std;
 
 //linkedlist Node
-struct listNode
-{
-	int data;
-//	listNode* prev;
-	listNode* next;
+struct listNode {
+    int data;
+//    listNode* prev;
+    listNode* next;
 };
 
 //constructor for new listNode
-listNode* newNode(int data)
-{
-	listNode* node = new listNode;
-	node->data = data;
-//	node->prev = NULL;
-	node->next = NULL;
-	return node;
+listNode* newNode(int data) {
+    listNode* node = new listNode;
+    node->data = data;
+//    node->prev = NULL;
+    node->next = NULL;
+    return node;
 }
 
 //construct linked list
-void construct_linkedList(listNode* &head)
-{
-	head					= newNode(1);
-	head->next				= newNode(2);
-	head->next->next		= newNode(3);
-	head->next->next->next	= newNode(4);
-	return;
+void construct_linkedList(listNode* &head) {
+    head                   = newNode(1);
+    head->next             = newNode(2);
+    head->next->next       = newNode(3);
+    head->next->next->next = newNode(4);
+    return;
 }
 
 //print linkedlist
-void print_linkedList(listNode* &head)
-{
-	if (head == NULL)
-		return;
+void print_linkedList(listNode* &head) {
+    if (head == NULL)
+        return;
 
-	cout << head->data << " ";
-	print_linkedList(head->next);
+    cout << head->data << " ";
+    print_linkedList(head->next);
 }
 
 //reverse linkedlist iteratively
-void iterative_reverse(listNode* &head)
-{
-	if ((head == NULL) || (head->next == NULL))
-		return;
+void iterative_reverse(listNode* &head) {
+    if ((head == NULL) || (head->next == NULL))
+        return;
 
-	listNode* temp = head->next;
-	listNode* temp1 = NULL;
+    listNode* temp = head->next;
+    listNode* temp1 = NULL;
 
-	while(head->next != NULL)
-	{
-		head->next = temp1;
-		temp1 = head;
-		head = temp;
-		temp = temp->next;		
-	}
-	head->next = temp1;
+    while(head->next != NULL) {
+        head->next = temp1;
+        temp1 = head;
+        head = temp;
+        temp = temp->next;        
+    }
+    head->next = temp1;
 
-	return;
+    return;
 }
 
-int main()
-{
-	//make head
-	listNode* head;
-	cout << "constructing linked list" << endl;
-	construct_linkedList(head);
-	print_linkedList(head);
-	cout << endl;
-	cout << "reversing linked list" << endl;
-	iterative_reverse(head);
-	print_linkedList(head);
-	cout << endl;
-	return 0;
+int main() {
+    //make head
+    listNode* head;
+    cout << "constructing linked list" << endl;
+    construct_linkedList(head);
+    print_linkedList(head);
+    cout << endl;
+    cout << "reversing linked list" << endl;
+    iterative_reverse(head);
+    print_linkedList(head);
+    cout << endl;
+    return 0;
 }
