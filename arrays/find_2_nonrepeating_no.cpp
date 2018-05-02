b7be2df207f7bb47423250334d25cb839b533776
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-05-01 20:00:21 -0500

    fixed indentation

--- arrays/find_2_nonrepeating_no.cpp
+++ arrays/find_2_nonrepeating_no.cpp
@@ -6,40 +6,38 @@
 
 using namespace std;
 
-void printNonRepeating(int arr[], int size)
-{
-	int xor_sum = arr[0];			// xor of all elements
-	int set_bit_no;				// Will have only single set bit of xor
-	int i;
-	int x = 0, y = 0;
+void printNonRepeating(int arr[], int size) {
+    int xor_sum = arr[0];          // xor of all elements
+    int set_bit_no;                // Will have only single set bit of xor
+    int i;
+    int x = 0, y = 0;
 
-	// Get the xor of all elements in arr[] and {1, 2 .. n}
-	for(i = 1; i < size; i++)
-		xor_sum ^= arr[i];
+    // Get the xor of all elements in arr[] and {1, 2 .. n}
+    for(i = 1; i < size; i++)
+        xor_sum ^= arr[i];
 
-	//cout << xor_sum << endl;
+    //cout << xor_sum << endl;
 
-	// Get the rightmost set bit in set_bit_no
-	set_bit_no = xor_sum & ~(xor_sum-1);
+    // Get the rightmost set bit in set_bit_no
+    set_bit_no = xor_sum & ~(xor_sum-1);
 
-	//cout << set_bit_no << endl;
+    //cout << set_bit_no << endl;
 
-	// divide elements in two sets by comparing rightmost set bit of xor with bit at same position in each element
-	for(i = 0; i < size; i++)
-	{
-		if(arr[i] & set_bit_no)
-			x = x ^ arr[i];				// XOR of first set in arr[]
-		else
-			y = y ^ arr[i];				// XOR of second set in arr[]
-	}
+    // divide elements in two sets by comparing rightmost set bit of xor with bit at same position in each element
+    for(i = 0; i < size; i++) {
+        if(arr[i] & set_bit_no)
+            x = x ^ arr[i];                // XOR of first set in arr[]
+        else
+            y = y ^ arr[i];                // XOR of second set in arr[]
+    }
 
-	cout << "The two repeating elements are " << x << " " << y << endl;
+    cout << "The two repeating elements are " << x << " " << y << endl;
 }
 
-int main()
-{
-	int arr[] = {4, 2, 4, 5, 2, 3, 1, 5};
-	int arr_size = sizeof(arr)/sizeof(arr[0]);
-	printNonRepeating(arr, arr_size);
-	return 0;
+int main() {
+    int arr[] = {4, 2, 4, 5, 2, 3, 1, 5};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    printNonRepeating(arr, arr_size);
+    return 0;
 }
+
