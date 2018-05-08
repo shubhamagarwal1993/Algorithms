8bf7ce87b0d9ccb029cd43f7483bfed6792a8673
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-05-07 20:57:10 -0500

    tab to spaces and indentation

--- strings/anagram_substring_search.cpp
+++ strings/anagram_substring_search.cpp
@@ -1,44 +1,83 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
 
 #define ASCII_MAX 256
+
 using namespace std;
 
 bool compareArrays(char arr1[], char arr2[]) {
-    for (int i = 0; i < ASCII_MAX; i++) {
-        if (arr1[i] != arr2[i]) {
+    for(int i = 0; i < ASCII_MAX; i++) {
+        if(arr1[i] != arr2[i]) {
             return false;
         }
     }
     return true;
 }
 
+bool compareMaps(unordered_map<char, int> map1, unordered_map<char, int> map2) {
+    return map1 == map2;
+}
+
+// Have used a map and an array to show that both can be used to solve this
 void searchPattern(char input[], int input_size, char pat[], int pattern_size) {
 
-    // pattern_arr: Store count of all characters of pattern
-    // input_arr  : Store count of current window of text
+    // pattern_count_arr: Store count of all characters of pattern
+    // input_count_arr  : Store count of current window of text
+    unordered_map<char, int> pattern_count_map;
+    unordered_map<char, int> window_count_map;
+
     char pattern_count_arr[ASCII_MAX] = {0};
     char window_count_arr[ASCII_MAX] = {0};
 
-    for (int i = 0; i < pattern_size; i++) {
+    for(int i = 0; i < pattern_size; i++) {
         (pattern_count_arr[pat[i]])++;
         (window_count_arr[input[i]])++;
+        pattern_count_map[pat[i]]++;
+        window_count_map[pat[i]]++;
     }
 
+    for(auto it = window_count_map.begin(); it != pattern_count_map.end(); ++it) {
+        cout << it->first << " -> " << it->second << endl;
+    }
+    cout << endl;
+
     // Traverse through remaining characters of pattern
-    for (int i = pattern_size; i < input_size; i++) {
-        if (compareArrays(pattern_count_arr, window_count_arr))
-            cout << "Found at Index " << (i - pattern_size) << endl;
+    for(int i = pattern_size; i < input_size; i++) {
+        if(compareArrays(pattern_count_arr, window_count_arr))
+            cout << "Arr Found at Index " << (i - pattern_size) << endl;
+
+        if(compareMaps(pattern_count_map, window_count_map))
+            cout << "Map Found at Index " << (i - pattern_size) << endl;
 
         // Add current character to current window
         (window_count_arr[input[i]])++;
 
+        // find a key
+        auto search = window_count_map.find(input[i]);
+        if(search != window_count_map.end())
+            window_count_map[input[i]]++;
+        else
+            window_count_map[input[i]] = 1;
+
         // Remove the first character of previous window
         window_count_arr[input[i-pattern_size]]--;
+
+        search = window_count_map.find(input[i-pattern_size]);
+        if(search != window_count_map.end()) {
+            int count = window_count_map[input[i-pattern_size]];
+            if(count == 1) {
+                window_count_map.erase(input[i-pattern_size]);
+            } else {
+                window_count_map[input[i-pattern_size]] = count - 1;
+            }
+        }
     }
 
     // Check for the last window in text
-    if (compareArrays(pattern_count_arr, window_count_arr))
-        cout << "Found at Index " << (input_size - pattern_size) << endl;
+    if(compareArrays(pattern_count_arr, window_count_arr))
+        cout << "Arr Found at Index " << (input_size - pattern_size) << endl;
+    if(compareMaps(pattern_count_map, window_count_map))
+        cout << "Map Found at Index " << (input_size - pattern_size) << endl;
 }
 
 int main() {
--- strings/combine_2_strings.cpp
+++ strings/combine_2_strings.cpp
@@ -8,116 +8,106 @@
 
 using namespace std;
 
-bool iscombined_iterative(string str1, string str2, string str3)
-{
-	if (str1.length() + str2.length() != str3.length())
-		return false;
+bool iscombined_iterative(string str1, string str2, string str3) {
+    if(str1.length() + str2.length() != str3.length())
+        return false;
 
-	if (str1.length() == 0 || str2.length() == 0)
-	{
-		if (str3 == str1 + str2)
-			return true;
-		else
-			return false;
-	}
-	
-	int j = 0, k = 0;			//j for str1, k for str2
+    if(str1.length() == 0 || str2.length() == 0) {
+        if (str3 == str1 + str2)
+            return true;
+        else
+            return false;
+    }
 
-	for (int i = 0; i < str3.length(); i++)
-	{
-		if (str3[i] == str1[j])
-		{
-			j++;
-			continue;
-		}
-		else if (str3[i] == str2[k])
-		{
-			k++;
-			continue;
-		}
-		else
-			return false;
-	}
-	
-	return true;
+    int j = 0, k = 0;            //j for str1, k for str2
+
+    for(int i = 0; i < str3.length(); i++) {
+        if(str3[i] == str1[j]) {
+            j++;
+            continue;
+        } else if(str3[i] == str2[k]) {
+            k++;
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 }
 
+bool iscombined_recursion(string str1, string str2, string str3) {
+    if(str1.length() + str2.length() != str3.length())
+        return false;
 
-bool iscombined_recursion(string str1, string str2, string str3)
-{
-	if (str1.length() + str2.length() != str3.length())
-		return false;
+    if(str1.length() == 0 || str2.length() == 0) {
+        if(str3 == str1 + str2)
+            return true;
+        else
+            return false;
+    }
 
-	if (str1.length() == 0 || str2.length() == 0)
-	{
-		if (str3 == str1 + str2)
-			return true;
-		else
-			return false;
-	}
+    if(str1[0] != str3[0] && str2[0] != str3[0])
+        return false;
 
-	if(str1[0] != str3[0] && str2[0] != str3[0])
-		return false;
+    if(str1[0] == str3[0] && iscombined_recursion(str1.substr(1), str2, str3.substr(1)))
+        return true;
 
-	if (str1[0] == str3[0] && iscombined_recursion(str1.substr(1), str2, str3.substr(1)))
-		return true;
+    if(str2[0] == str3[0] && iscombined_recursion(str1, str2.substr(1), str3.substr(1)))
+        return true;
 
-	if (str2[0] == str3[0] && iscombined_recursion(str1, str2.substr(1), str3.substr(1)))
-		return true;
-
-	return false;
+    return false;
 }
+
 /*
-bool iscombined_dynamic(string str1, string str2, string str3, set<int> myset)
-{
-	if (str1.length() + str2.length() != str3.length())
-		return false;
+bool iscombined_dynamic(string str1, string str2, string str3, set<int> myset) {
+    if(str1.length() + str2.length() != str3.length())
+        return false;
 
-	if (str1.length() == 0 || str2.length() == 0)
-	{
-		if (str3 == str1 + str2)
-			return true;
-		else
-			return false;
-	}
+    if(str1.length() == 0 || str2.length() == 0) {
+        if (str3 == str1 + str2)
+            return true;
+        else
+            return false;
+    }
 
-	if(str1[0] != str3[0] && str2[0] != str3[0])
-		return false;
+    if(str1[0] != str3[0] && str2[0] != str3[0])
+        return false;
 
-	if (str1[0] == str3[0] && iscombined_dynamic(str1.substr(1), str2, str3.substr(1), myset))
-		return true;
+    if (str1[0] == str3[0] && iscombined_dynamic(str1.substr(1), str2, str3.substr(1), myset))
+        return true;
 
-	if (str2[0] == str3[0] && iscombined_dynamic(str1, str2.substr(1), str3.substr(1), myset))
-		return true;
+    if (str2[0] == str3[0] && iscombined_dynamic(str1, str2.substr(1), str3.substr(1), myset))
+        return true;
 
-	return false;
+    return false;
 }
 */
-int main()
-{
+
+int main() {
 /*
-	//using recursion only
-	string str1 = "abc";
-	string str2 = "def";
-	string str3 = "dacbef";
-	cout << iscombined_recursion(str1, str2, str3) << endl;
+    //using recursion only
+    string str1 = "abc";
+    string str2 = "def";
+    string str3 = "dacbef";
+    cout << iscombined_recursion(str1, str2, str3) << endl;
 */
-/*	DONT SEE THE POINT OF DYNAMIC HERE -> NOT WORKING AS WELL.
-	//using dynamic
-	string str1 = "abc";
-	string str2 = "def";
-	string str3 = "dacbef";
+/*    DONT SEE THE POINT OF DYNAMIC HERE -> NOT WORKING AS WELL.
+    //using dynamic
+    string str1 = "abc";
+    string str2 = "def";
+    string str3 = "dacbef";
 
-	set<int> myset;
-	cout << iscombined_dynamic(str1, str2, str3, myset) << endl;	
+    set<int> myset;
+    cout << iscombined_dynamic(str1, str2, str3, myset) << endl;    
 */
 
-	//using iterative method
-	string str1 = "abc";
-	string str2 = "dbf";
-	string str3 = "dabbcf";
-	cout << iscombined_iterative(str1, str2, str3) << endl;
-	
-	return 0;
+    //using iterative method
+    string str1 = "abc";
+    string str2 = "dbf";
+    string str3 = "dabbcf";
+    cout << iscombined_iterative(str1, str2, str3) << endl;
+    
+    return 0;
 }
 
