2528c4c0a0de3d7aaa4f2b5be7d16c5fdb502ffc
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-05-07 21:04:17 -0500

    tab to spaces and indentation

--- strings/edit_distance.cpp
+++ strings/edit_distance.cpp
@@ -13,66 +13,68 @@ using namespace std;
 
 
 //simple recursion - trying all cases
-int edit_distance(string str1, string str2, int str1_len, int str2_len)
-{
-	if (str1_len == 0)
-		return str2_len;
+int edit_distance(string str1, string str2, int str1_len, int str2_len) {
+    if(str1_len == 0)
+        return str2_len;
 
-	if (str2_len == 0)
-		return str1_len;
+    if(str2_len == 0)
+        return str1_len;
 
-	//check if last character is same
-	if (str1[str1_len-1] == str2[str2_len-1])
-		return edit_distance(str1, str2, str1_len-1, str2_len-1);
+    //check if last character is same
+    if(str1[str1_len-1] == str2[str2_len-1])
+        return edit_distance(str1, str2, str1_len-1, str2_len-1);
 
-	//if not same character, then try all 3 combinations
-	return 1 + min( min(edit_distance(str1, str2, str1_len, str2_len-1), edit_distance(str1, str2, str1_len-1, str2_len)), edit_distance(str1, str2, str1_len-1, str2_len-1));
+    //if not same character, then try all 3 combinations
+    return 1 + min( 
+                min(
+                    edit_distance(str1, str2, str1_len, str2_len-1), 
+                    edit_distance(str1, str2, str1_len-1, str2_len)
+                    ), 
+                edit_distance(str1, str2, str1_len-1, str2_len-1)
+                );
 }
 
 //dynamic - using 2d array
-int edit_distance(string str1, string str2)
-{
-	int str1_len = str1.length();
-	int str2_len = str2.length();
+int edit_distance(string str1, string str2) {
 
-	int dp[str1_len+1][str2_len+1];
-//	dp[0][0] = 0;
+    int str1_len = str1.length();
+    int str2_len = str2.length();
 
-	// Fill d[][] in bottom up manner
-	for (int i = 0; i <= str1_len; i++)
-	{
-		for (int j = 0; j <= str2_len; j++)
-		{
-			if (i == 0)
-				dp[i][j] = j;
+    int dp[str1_len+1][str2_len+1];
+//    dp[0][0] = 0;
 
-			else if (j == 0)
-				dp[i][j] = i;
+    // Fill d[][] in bottom up manner
+    for(int i = 0; i <= str1_len; i++) {
+        for(int j = 0; j <= str2_len; j++) {
+            if (i == 0)
+                dp[i][j] = j;
 
-			else if (str1[i-1] == str2[j-1])
-				dp[i][j] = dp[i-1][j-1];
+            else if (j == 0)
+                dp[i][j] = i;
 
-			else
-				dp[i][j] = 1 + min(min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
-		}
-	}
+            else if (str1[i-1] == str2[j-1])
+                dp[i][j] = dp[i-1][j-1];
 
-	return dp[str1_len][str2_len];
+            else
+                dp[i][j] = 1 + min(min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+        }
+    }
+
+    return dp[str1_len][str2_len];
 }
 
-int main()
-{
+int main() {
 /*
-	//time O(3^m)
-	string str1 = "temp";
-	string str2 = "aaaa";
-	cout << edit_distance(str1, str2, str1.length(), str2.length()) << endl;
+    //time O(3^m)
+    string str1 = "temp";
+    string str2 = "aaaa";
+    cout << edit_distance(str1, str2, str1.length(), str2.length()) << endl;
 */
 
-	//using dynamic - time O(mn)
-	string str1 = "temper";
-	string str2 = "tamp";
-	cout << edit_distance(str1, str2) << endl;
-	return 0;
+    //using dynamic - time O(mn)
+    string str1 = "temper";
+    string str2 = "tamp";
+    cout << edit_distance(str1, str2) << endl;
+    return 0;
 }
 
