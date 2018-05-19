a9c2808e03c1b5556e17bf1500b9f5777d489118
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-05-19 17:17:22 -0500

    adding file for references to practise and brush up

---
+++ references/README.md
@@ -0,0 +1,4 @@
+#### Taking an interview - basic things to ask
+`https://sites.google.com/site/steveyegge2/five-essential-phone-screen-questions`
+
+#### 
--- strings/reverse_words_in_string.cpp
+++ strings/reverse_words_in_string.cpp
@@ -1,56 +1,48 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <set>
 
 using namespace std;
 
-void reverse(string &str, int left, int right)
-{
-	char temp;
+void reverse(string &str, int left, int right) {
 
-	if (str.length() == 0)
-		return;
+    if(str.empty()) {
+        return;
+    }
 
-	for (int i = 0; i < (right+1-left)/2; i++)
-	{
-		temp = str[left+i];
-		str[left+i] = 	str[right-i];
-		str[right-i] = temp;
-	}
-	return;
+    for(int i = 0; i < (right+1-left)/2; i++) {
+        char temp = str[left+i];
+        str[left+i] =     str[right-i];
+        str[right-i] = temp;
+    }
+
+    return;
 }
 
-void reverse_words(string &str)
-{
-	int l = 0, r = 0;
-	for(int i = 0; i < str.length(); i++)
-	{
-		if(str[i] == ' ')
-		{
-			r = i-1;
-			reverse(str, l, r);
-			l = i+1;
-		}
-		else if (i == str.length()-1)
-		{
-			r = i;
-			reverse(str, l, r);
-		}
-	}
-	return;
+void reverse_words(string &str) {
+
+    int l = 0;
+    int r = 0;
+
+    for(int i = 0; i < str.length(); i++) {
+
+        if(str[i] == ' ') {
+            r = i-1;
+            reverse(str, l, r);
+            l = i+1;
+        } else if(i == str.length()-1) {
+            r = i;
+            reverse(str, l, r);
+        }
+    }
+    return;
 }
 
-int main()
-{
-	string str = "abc dfg erwe";
-	reverse(str, 0, str.length()-1);
-	cout << str << endl;
-	reverse_words(str);
-	cout << str << endl;
-	return 0;
+int main() {
+    string str = "abc dfg erwe";
+    cout << "Original String: " << str << endl;
+    reverse(str, 0, str.length()-1);
+    cout << "Reversed String: " << str << endl;
+    reverse_words(str);
+    cout << "Reversed words : " << str << endl;
+    return 0;
 }
 
