2ef455566790dad1e0f56d718c48c1d587dde094
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-06-30 17:52:48 -0500

    moved functions to class

--- trees/construct_tree_from_level_in_order.cpp
+++ trees/construct_tree_from_level_in_order.cpp
@@ -6,140 +6,142 @@
 
 using namespace std;
 
-// tree node
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+// Tree node
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-// constructor for new node
-Node* newNode(int data) {
-    Node* node = new Node;
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
+class Tree {
+    public:
+        Node* root;
 
-int height(Node* node) {
-    if(node == NULL) {
-        return 0;
-    }
-    
-    // compute the height of each subtree
-    int lheight = height(node->left);
-    int rheight = height(node->right);
-
-    // use the larger one
-    if(lheight > rheight)
-        return(lheight+1);
-    else
-        return(rheight+1);
-}
-
-// Print nodes at a given level
-void printGivenLevel(Node* root, int level) {
-    if(root == NULL)
-        return;
-
-    if(level == 1)
-        cout << root->data << " ";
-
-    else if(level > 1) {
-        printGivenLevel(root->left, level-1);
-        printGivenLevel(root->right, level-1);
-    }
-}
-
-//print tree to check
-void printLevelOrder(Node* root) {
-    int h = height(root);
-    for(int i = 1; i <= h; i++)
-        printGivenLevel(root, i);
-}
-
-void in_order(Node* root) {
-    if(root == NULL)
-        return;
-
-    in_order(root->left);
-    cout << root->data << " ";
-    in_order(root->right);
-}
-
-int search(int inorder[], int in_start, int in_end, int data) {
-
-    for(int i = in_start; i <= in_end; i++) {
-        if(data == inorder[i]) {
-            return i;
+        Tree() {
+            this->root = NULL;
         }
-    }
 
-    return -1;
-}
+        int height(Node* node) {
+            if(node == NULL) {
+                return 0;
+            }
+            
+            // compute the height of each subtree
+            int lheight = height(node->left);
+            int rheight = height(node->right);
 
-int *extractKeys(int inorder[], int levelorder[], int in_index, int size) {
-    int *arr = new int [in_index];
-    int j = 0;
-    for(int i = 0; i < size; i++) {
-        if(search(inorder, 0, in_index-1, levelorder[i]) != -1) {
-            arr[j] = levelorder[i];
-            j++;
+            // use the larger one
+            return max(lheight, rheight) + 1;
         }
-    }
 
-    return arr;
-}
+        // Print nodes at a given level
+        void printGivenLevel(Node* root, int level) {
+            if(root == NULL)
+                return;
 
-// construct a bst using inorder & levelorder traversals.
-// inorder    - 5, 10, 20, 50, 51, 55, 60, 65, 70, 80
-// levelorder - 50, 10, 60, 5, 20, 55, 70, 51, 65, 80
-//         50
-//      /      \
-//    10        60
-//   /  \       /  \
-//  5   20    55    70
-//            /     /  \
-//          51     65    80
+            if(level == 1)
+                cout << root->data << " ";
 
-Node* construct_tree(int inorder[], int levelorder[], int in_start, int in_end, int size, int &level_index) {
+            else if(level > 1) {
+                printGivenLevel(root->left, level-1);
+                printGivenLevel(root->right, level-1);
+            }
+        }
 
-    if(in_start > in_end) {
-        return NULL;
-    }
+        //print tree to check
+        void printLevelOrder(Node* root) {
+            int h = height(root);
+            for(int i = 1; i <= h; i++)
+                printGivenLevel(root, i);
+        }
 
-    // The first node in level order traversal is root
-    Node* root = newNode(levelorder[level_index]);
-    level_index++;
+        void in_order(Node* root) {
+            if(root == NULL)
+                return;
 
-//    if(in_start == in_end) {
-//        return root;
-//    }
+            in_order(root->left);
+            cout << root->data << " ";
+            in_order(root->right);
+        }
 
-    // find index of root in inorder array
-    int in_index = search(inorder, in_start, in_end, root->data);
-    if(in_index == -1) {
-        return NULL;
-    }
+        int search(int inorder[], int in_start, int in_end, int data) {
 
-    // separate left keys from level order traversal - return an array
-//    int *left_index = extractKeys(inorder, levelorder, in_index, size);
+            for(int i = in_start; i <= in_end; i++) {
+                if(data == inorder[i]) {
+                    return i;
+                }
+            }
 
-    // separate right keys from level order traversal - return an array
-//    int *right_index = extractKeys(inorder + in_index + 1, levelorder, size - in_index - 1, size);
+            return -1;
+        }
 
-    //using in_index from inorder array, constructing left & right subtrees.
-//    root->left  = construct_tree(inorder, left_index, in_start, in_index-1, size);
-//    root->right = construct_tree(inorder, right_index, in_index+1, in_end, size);
+        int *extractKeys(int inorder[], int levelorder[], int in_index, int size) {
+            int *arr = new int [in_index];
+            int j = 0;
+            for(int i = 0; i < size; i++) {
+                if(search(inorder, 0, in_index-1, levelorder[i]) != -1) {
+                    arr[j] = levelorder[i];
+                    j++;
+                }
+            }
 
-    root->left  = construct_tree(inorder, levelorder, in_start, in_index-1, size, level_index);
-    root->right = construct_tree(inorder, levelorder, in_index+1, in_end, size, level_index);
-//    delete [] left_index;
-//    delete [] right_index;
+            return arr;
+        }
 
-    return root;
-}
+        // construct a bst using inorder & levelorder traversals.
+        // inorder    - 5, 10, 20, 50, 51, 55, 60, 65, 70, 80
+        // levelorder - 50, 10, 60, 5, 20, 55, 70, 51, 65, 80
+        //         50
+        //      /      \
+        //    10        60
+        //   /  \       /  \
+        //  5   20    55    70
+        //            /     /  \
+        //          51     65    80
+        Node* construct_tree(int inorder[], int levelorder[], int in_start, int in_end, int size, int &level_index) {
+
+            if(in_start > in_end) {
+                return NULL;
+            }
+
+            // The first node in level order traversal is root
+            Node* root = new Node(levelorder[level_index]);
+            level_index++;
+
+        //    if(in_start == in_end) {
+        //        return root;
+        //    }
+
+            // find index of root in inorder array
+            int in_index = search(inorder, in_start, in_end, root->data);
+            if(in_index == -1) {
+                return NULL;
+            }
+
+            // separate left keys from level order traversal - return an array
+        //    int *left_index = extractKeys(inorder, levelorder, in_index, size);
+
+            // separate right keys from level order traversal - return an array
+        //    int *right_index = extractKeys(inorder + in_index + 1, levelorder, size - in_index - 1, size);
+
+            //using in_index from inorder array, constructing left & right subtrees.
+        //    root->left  = construct_tree(inorder, left_index, in_start, in_index-1, size);
+        //    root->right = construct_tree(inorder, right_index, in_index+1, in_end, size);
+
+            root->left  = construct_tree(inorder, levelorder, in_start, in_index-1, size, level_index);
+            root->right = construct_tree(inorder, levelorder, in_index+1, in_end, size, level_index);
+        //    delete [] left_index;
+        //    delete [] right_index;
+
+            return root;
+        }
+};
 
 int main() {
 
@@ -150,8 +152,9 @@ int main() {
     int in_end = size - 1;
     int level_index = 0;
 
-    Node* root = construct_tree(inorder, levelorder, in_start, in_end, size, level_index);
-    in_order(root);
+    Tree tree;
+    tree.root = tree.construct_tree(inorder, levelorder, in_start, in_end, size, level_index);
+    tree.in_order(tree.root);
     cout << endl;
 
     return 0;
--- trees/delete_node.cpp
+++ trees/delete_node.cpp
@@ -3,129 +3,140 @@
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-Node* newNode(int data) {
-    Node* root = new Node();
-    root->data = data;
-    root->left = NULL;
-    root->right = NULL;
-    return root;
-}
+class Tree {
+    public:
+        Node* root;
 
-Node* constructTree() {
-    Node* root = newNode(1);
-
-    root->left = newNode(2);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-
-    root->right = newNode(3);
-    root->right->right = newNode(8);
-    root->right->right->left = newNode(6);
-    root->right->right->right = newNode(7);
-    return root;
-}
-
-Node* getInorderSuccessor(Node* root) {
-    if(root == NULL) {
-        return root;
-    }
-
-    while(root->left != NULL) {
-        root = root->left;
-    }
-    return root; 
-}
-
-Node* deleteNode(Node* root, int data) {
-    if(root == NULL) {
-        return root;
-    }
-
-    if(data < root->data) {
-        root->left = deleteNode(root->left, data);
-    } else if(data > root->data) {
-        root->right = deleteNode(root->right, data);
-    // We are at the node to delete
-    } else {
-        // If no child
-        if(root->left == NULL && root->right == NULL) {
-            delete root;
-            root = NULL;
-        // if no left child, delete root and return right child
-        } else if(root->left == NULL) {
-            Node* temp = root->right;
-            delete(root);
-            return temp;
-        // if no right child, delete root and retunn left child
-        } else if(root->right == NULL) {
-            Node* temp = root->left;
-            delete(root);
-            return temp;
-        // Both child
-        } else {
-            Node* temp = getInorderSuccessor(root->right);
-            root->data = temp->data;
-            root->right = deleteNode(root->right, root->data);
+        Tree() {
+            this->root = NULL;
         }
-    }
 
-    return root;
-}
+        Node* constructTree() {
+            Node* root = new Node(1);
 
-int height(Node* node) {
-    if ( node == NULL) {
-        return 0;
-    } else {
-        // compute the height of each subtree
-        int lheight = height(node->left);
-        int rheight = height(node->right);
+            root->left = new Node(2);
+            root->left->left = new Node(4);
+            root->left->right = new Node(5);
 
-        // use the larger one
-        if (lheight > rheight) {
-            return(lheight+1);
-        } else {
-            return(rheight+1);
+            root->right = new Node(3);
+            root->right->right = new Node(8);
+            root->right->right->left = new Node(6);
+            root->right->right->right = new Node(7);
+            return root;
         }
-    }
-}
 
-// Print nodes at a given level
-void printGivenLevel(Node* root, int level) {
-    if (root == NULL) {
-        return;
-    }
+        Node* getInorderSuccessor(Node* root) {
+            if(root == NULL) {
+                return root;
+            }
 
-    if (level == 1) {
-        cout << root->data << " ";
-    } else if (level > 1) {
-        printGivenLevel(root->left, level-1);
-        printGivenLevel(root->right, level-1);
-    }
-}
+            while(root->left != NULL) {
+                root = root->left;
+            }
+            return root; 
+        }
 
-//print tree to check
-void printLevelOrder(Node* root) {
-    int h = height(root);
-    for (int i = 1; i <= h; i++) {
-        printGivenLevel(root, i);
-        cout << endl;
-    }
+        Node* deleteNode(Node* root, int data) {
+            if(root == NULL) {
+                return root;
+            }
 
-}
+            if(data < root->data) {
+                root->left = deleteNode(root->left, data);
+            } else if(data > root->data) {
+                root->right = deleteNode(root->right, data);
+            // We are at the node to delete
+            } else {
+                // If no child
+                if(root->left == NULL && root->right == NULL) {
+                    delete root;
+                    root = NULL;
+                // if no left child, delete root and return right child
+                } else if(root->left == NULL) {
+                    Node* temp = root->right;
+                    delete(root);
+                    return temp;
+                // if no right child, delete root and retunn left child
+                } else if(root->right == NULL) {
+                    Node* temp = root->left;
+                    delete(root);
+                    return temp;
+                // Both child
+                } else {
+                    Node* temp = getInorderSuccessor(root->right);
+                    root->data = temp->data;
+                    root->right = deleteNode(root->right, root->data);
+                }
+            }
+
+            return root;
+        }
+
+        int height(Node* node) {
+            if (node == NULL) {
+                return 0;
+            } else {
+                // compute the height of each subtree
+                int lheight = height(node->left);
+                int rheight = height(node->right);
+
+                // use the larger one
+                if (lheight > rheight) {
+                    return(lheight+1);
+                } else {
+                    return(rheight+1);
+                }
+            }
+        }
+
+        // Print nodes at a given level
+        void printGivenLevel(Node* root, int level) {
+            if (root == NULL) {
+                return;
+            }
+
+            if (level == 1) {
+                cout << root->data << " ";
+            } else if (level > 1) {
+                printGivenLevel(root->left, level-1);
+                printGivenLevel(root->right, level-1);
+            }
+        }
+
+        //print tree to check
+        void printLevelOrder(Node* root) {
+            int h = height(root);
+            for (int i = 1; i <= h; i++) {
+                printGivenLevel(root, i);
+                cout << endl;
+            }
+
+        }
+};
 
 int main() {
-    Node* root = constructTree();
-    printLevelOrder(root);
+    Tree tree;
+    tree.root = tree.constructTree();
+    tree.printLevelOrder(tree.root);
+
+    cout << "----------" << endl;
 
     // Delete node in O(n) time
-    root = deleteNode(root, 3);
-    printLevelOrder(root);
+    tree.root = tree.deleteNode(tree.root, 3);
+    tree.printLevelOrder(tree.root);
 
     return 0;
 }
