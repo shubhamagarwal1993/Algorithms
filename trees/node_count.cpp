1320da267d7cbcd029f1d3749bb1b6f3a4865d48
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-06-30 10:15:38 -0500

    moved functions to class

--- trees/node_count.cpp
+++ trees/node_count.cpp
@@ -3,66 +3,75 @@
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-Node* newNode(int data) {
-    Node* root = new Node();
-    root->data = data;
-    root->left = NULL;
-    root->right = NULL;
-    return root;
-}
+class Tree {
+    public:
+        Node* root;
 
-Node* constructTree() {
-    Node* root = newNode(1);
+        Tree() {
+            this->root = NULL;
+        }
 
-    root->left = newNode(2);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
+        Node* constructTree() {
+            Node* root = new Node(1);
 
-    root->right = newNode(3);
-    root->right->right = newNode(8);
-    root->right->right->left = newNode(6);
-    root->right->right->right = newNode(7);
-    return root;
-//              1
-//             / \
-//            2   3
-//           / \   \
-//          4   5   8
-//                 / \
-//                6   7
-}
+            root->left = new Node(2);
+            root->left->left = new Node(4);
+            root->left->right = new Node(5);
 
-int countBelowEachNode(Node* root) {
+            root->right = new Node(3);
+            root->right->right = new Node(8);
+            root->right->right->left = new Node(6);
+            root->right->right->right = new Node(7);
+            return root;
+        //              1
+        //             / \
+        //            2   3
+        //           / \   \
+        //          4   5   8
+        //                 / \
+        //                6   7
+        }
 
-    if(root == NULL) {
-        return 0;
-    }
+        int countBelowEachNode(Node* root) {
 
-    int left_count = countBelowEachNode(root->left);
-    int right_count = countBelowEachNode(root->right);
+            if(root == NULL) {
+                return 0;
+            }
 
-    if(root->left != NULL) {
-        left_count += 1;
-    }
-    if(root->right != NULL) {
-        right_count += 1;
-    }
+            int left_count = countBelowEachNode(root->left);
+            int right_count = countBelowEachNode(root->right);
 
-    cout << root->data << " " << left_count + right_count << endl;
-    return left_count + right_count;
-}
+            if(root->left != NULL) {
+                left_count += 1;
+            }
+            if(root->right != NULL) {
+                right_count += 1;
+            }
+
+            cout << root->data << " " << left_count + right_count << endl;
+            return left_count + right_count;
+        }
+};
 
 int main() {
-    Node* root = constructTree();
+    Tree tree;
+    tree.root = tree.constructTree();
 
     // O(n) time and O(1) space
-    countBelowEachNode(root);
+    tree.countBelowEachNode(tree.root);
 
     return 0;
 }
