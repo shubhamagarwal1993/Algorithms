9f3f4b4ed5de02373f6b6d818362cc9a2bc4a95d
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-06-29 19:34:05 -0500

    lca move functions into class

--- trees/LCA.cpp
+++ trees/LCA.cpp
@@ -3,101 +3,124 @@
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-Node* newNode(int data) {
-    Node* root = new Node();
-    root->data = data;
-    root->left = NULL;
-    root->right = NULL;
-    return root;
-}
+class Tree {
+    public:
+        Node* root;
 
-Node* constructTree() {
-    Node* root = newNode(8);
+        Tree() {
+            this->root = NULL;
+        }
 
-    root->left = newNode(3);
-    root->left->left = newNode(1);
-    root->left->right = newNode(6);
-    root->left->right->left = newNode(4);
-    root->left->right->right = newNode(7);
+        Node* constructTree() {
+            Node* root = new Node(8);
 
-    root->right = newNode(12);
-    root->right->left = newNode(11);
-    root->right->right = newNode(15);
-    root->right->right->left = newNode(14);
+            root->left = new Node(3);
+            root->left->left = new Node(1);
+            root->left->right = new Node(6);
+            root->left->right->left = new Node(4);
+            root->left->right->right = new Node(7);
 
-    return root;
-}
+            root->right = new Node(12);
+            root->right->left = new Node(11);
+            root->right->right = new Node(15);
+            root->right->right->left = new Node(14);
 
-Node* findLCAUtil(Node* root, int node1, bool &node1_exists, int node2, bool &node2_exists) {
+            return root;
+        }
 
-    Node* test = NULL;
-    if(root == NULL) {
-        return NULL;
-    }
+        void findLCA(Node* root, int node1, int node2) {
+            if(root == NULL) {
+                return;
+            }
 
-    if(root->data == node1) {
-        node1_exists = true;
-        test = root;
-    }
+            bool node1_exists = false;
+            bool node2_exists = false;
 
-    if(root->data == node2) {
-        node2_exists = true;
-        test = root;
-    }
+            Node* lca = findLCAUtil(root, node1, node1_exists, node2, node2_exists);
+            if(node1_exists && node2_exists) {
+                cout << "Found LCA: " << lca->data << endl;
+            } else if(node1_exists && !node2_exists) {
+                cout << node2 << " does not exist" << endl;
+            } else if(!node1_exists && node2_exists) {
+                cout << node1 << " does not exist" << endl;
+            } else {
+                cout << node1 << " and " << node2 << " do not exist" << endl;
+            }
 
-    if(node1_exists && node2_exists && test != NULL) {
-        return test;
-    }
+            return;
+        }
 
-    Node* LCALeft = findLCAUtil(root->left, node1, node1_exists, node2, node2_exists);
-    Node* LCARight = findLCAUtil(root->right, node1, node1_exists, node2, node2_exists);
+        Node* findLCAUtil(Node* root, int node1, bool &node1_exists, int node2, bool &node2_exists) {
 
-    // check if temp is set, which means LCA was found
-    if(test != NULL) {
-        return test;
-    }
+            Node* test = NULL;
+            if(root == NULL) {
+                return NULL;
+            }
 
-    if(LCALeft == NULL && LCARight == NULL) {
-        return NULL;
-    } else if(LCALeft != NULL && LCARight != NULL) {
-        return root;
-    } else {
-        return (LCALeft != NULL) ? LCALeft : LCARight;
-    }
-}
+            if(root->data == node1) {
+                node1_exists = true;
+                test = root;
+            }
 
-void findLCA(Node* root, int node1, int node2) {
-    if(root == NULL) {
-        return;
-    }
+            if(root->data == node2) {
+                node2_exists = true;
+                test = root;
+            }
 
-    bool node1_exists = false;
-    bool node2_exists = false;
+            if(node1_exists && node2_exists && test != NULL) {
+                return test;
+            }
 
-    Node* lca = findLCAUtil(root, node1, node1_exists, node2, node2_exists);
-    if(node1_exists && node2_exists) {
-        cout << "Found LCA: " << lca->data << endl;
-    } else if(node1_exists && !node2_exists) {
-        cout << node2 << " does not exist" << endl;
-    } else if(!node1_exists && node2_exists) {
-        cout << node1 << " does not exist" << endl;
-    } else {
-        cout << node1 << " and " << node2 << " do not exist" << endl;
-    }
+            Node* LCALeft = findLCAUtil(root->left, node1, node1_exists, node2, node2_exists);
+            Node* LCARight = findLCAUtil(root->right, node1, node1_exists, node2, node2_exists);
 
-    return;
-}
+            // check if temp is set, which means LCA was found
+            if(test != NULL) {
+                return test;
+            }
+
+            if(LCALeft == NULL && LCARight == NULL) {
+                return NULL;
+            } else if(LCALeft != NULL && LCARight != NULL) {
+                return root;
+            } else {
+                return (LCALeft != NULL) ? LCALeft : LCARight;
+            }
+        }
+
+        void destroyTree(Node* root) {
+            if(root == NULL) {
+                return;
+            }
+
+            destroyTree(root->left);
+            destroyTree(root->right);
+            root->left = NULL;
+            root->right = NULL;
+            delete root;
+        }
+};
 
 int main() {
-    Node* root = constructTree();
-    findLCA(root, 14, 11);
-    findLCA(root, 8, 3);
+    Tree tree;
+    tree.root = tree.constructTree();
+    tree.findLCA(tree.root, 14, 11);
+    tree.findLCA(tree.root, 8, 3);
+    tree.destroyTree(tree.root);
+    tree.root = NULL;
 
     return 0;
 }
