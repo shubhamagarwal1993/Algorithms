0c6b79ad9cb6ae82eade75b67da0097466a4040c
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-07-14 11:58:50 -0500

    min cost path

--- backtracking/maze_min_cost_path.cpp
+++ backtracking/maze_min_cost_path.cpp
@@ -4,6 +4,61 @@
 #define N 3
 using namespace std;
 
+// check if we can move to a particular cell
+bool validCell(int r_idx, int c_idx) {
+    // invalid M index      
+    if (r_idx < 0 || r_idx >= M) return false;
+
+    // inavlid N index
+    if (c_idx < 0 || c_idx >= N) return false;
+
+    return true;
+}
+
+bool printMinCostPath(int testMaze[M][N], int r, int c) {
+    if((r == 0) && (c == 0)) {
+        cout << r << "-" << c << endl;
+        return true;
+    }
+
+    if (validCell(r, c)) {
+
+        cout << r << "-" << c << endl;
+
+        int min_r = -1;
+        int min_c = -1;
+        int min_cost = INT_MAX;
+
+        // find min in neighbors
+        if(validCell(r-1, c) && (testMaze[r-1][c] < min_cost)) {
+            min_cost = testMaze[r-1][c];
+            min_r = r-1;
+            min_c = c;
+        }
+
+        if(validCell(r, c-1) && (testMaze[r][c-1] < min_cost)) {
+            min_cost = testMaze[r][c-1];
+            min_r = r;
+            min_c = c-1;
+        }
+
+        if(validCell(r-1, c-1) && (testMaze[r-1][c-1] < min_cost)) {
+            min_cost = testMaze[r-1][c-1];
+            min_r = r-1;
+            min_c = c-1;
+        }
+
+        // recursive call on smallest cost path
+        if(printMinCostPath(testMaze, min_r, min_c)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return false;
+}
+
 void minPathInMaze(int maze[M][N]) {
     int row = N;
     int col = M;
@@ -39,6 +94,9 @@ void minPathInMaze(int maze[M][N]) {
     cout << "---------" << endl;
 
     cout << "Min cost to reach end of maze: " << testMaze[M-1][N-1] << endl;
+
+    // print path coordinates
+    printMinCostPath(testMaze, M-1, N-1);
 }
 
 // main
