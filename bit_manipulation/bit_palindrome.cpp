f80c01f92a83e0edfd89803b58c33bd824dfbe78
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-04 22:24:56 -0500

    Added condition to check for trailing 0s while checking for bit palindrome

--- bit_manipulation/bit_palindrome.cpp
+++ bit_manipulation/bit_palindrome.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-bool kthBitIs1(unsigned int num, int k) {
+bool bitValueAtK(unsigned int num, int k) {
     int mask = 1 << k;
     int result = mask & num;
     return result != 0 ? true : false;
@@ -10,18 +10,36 @@ bool kthBitIs1(unsigned int num, int k) {
 
 bool isPalindrome(unsigned int num) {
 
-    int num_temp = num;
+    // assume long int
+    int int_max_bits = 32;
+
+    // get total bits num needs, excluding leading 0s
     int total_bits = 0;
+    int num_temp = num;
     while(num_temp != 0) {
         total_bits++;
         num_temp = num_temp >> 1;
     }
 
+    // count ending 0s
+    int total_trailing_0s = 0;
+    num_temp = num;
+    while(!(1 & num_temp)) {
+        total_trailing_0s++;
+        num_temp = num_temp >> 1;
+    }
+
+    // Check if we have enough space to put 0s in the front of the num
+    if(total_bits + total_trailing_0s > int_max_bits) {
+        return false;
+    }
+
+    // set pointers on num
     int right = 0;
-    int left = total_bits - 1;
+    int left = total_bits + total_trailing_0s - 1;
 
     while(left > right) {
-        if(kthBitIs1(num, right) != kthBitIs1(num, left)) {
+        if(bitValueAtK(num, right) != bitValueAtK(num, left)) {
             return false;
         }
         right++;
@@ -34,6 +52,13 @@ bool isPalindrome(unsigned int num) {
 int main() {
 
     // binary representation 0000010001010001
-    cout << isPalindrome(1105) << endl;
+    //int num = 1105;
+    int num = 8840;
+    if(isPalindrome(num)) {
+        cout << "bit representation of " << num << " is a palindrome" << endl;
+    } else {
+        cout << "bit representation of " << num << " is not a palindrome" << endl;
+    }
+
     return 0;
 }
