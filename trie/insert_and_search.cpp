5785078745d220bb52db3db3381028e59278f721
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-10 09:43:33 -0500

    removed basic math txt file

--- trie/insert_and_search.cpp
+++ trie/insert_and_search.cpp
@@ -7,184 +7,210 @@
 //Search - O(M)                                -M is key length
 //Memory - O(ALPHABET_SIZE * M * N)            -M is key length, N is number of keys in Trie
 
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 #define ALPHABET_SIZE 26
 int flag = 1;
 
-//tree node
-struct TrieNode {
-    std::map<char, TrieNode*> mymap;
-    bool isLeaf;
+class TrieNode {
+    public:
+        unordered_map<char, TrieNode*> mymap;
+        bool isLeaf;
+
+        TrieNode() {
+            this->isLeaf = false;
+        }
 };
 
-//constructor for new node
-TrieNode* newTrieNode() {
-    TrieNode* trieNode = new TrieNode;
-    trieNode->mymap.clear();
-    trieNode->isLeaf = false;        
-    return trieNode;
-}
+class Trie {
+    public:
+        TrieNode* root;
 
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just marks leaf node
-void insert(TrieNode *root, string key) {
-    int level = 0;
-
-    // root already has a map defined
-    for(int i = 0; i < key.length(); i++) {
-        level++;
-        char index = key[i];
-        std::map<char, TrieNode*>::iterator it;
-        it = root->mymap.find(index);
-
-        //found char, move on to next char
-        if(it != root->mymap.end()) {
-            root = root->mymap[index];
-        // new character
-        } else {
-            TrieNode* temp = newTrieNode();
-            root->mymap[index] = temp;
-            if (level < key.length())
-                root = root->mymap[index];
+        Trie() {
+            this->root = NULL;
         }
-    }
 
-    // mark last node as leaf
-    root->isLeaf = true;
-}
+        // If not present, inserts key into trie
+        // If the key is prefix of trie node, just marks leaf node
+        void insert(string key) {
 
-// Prints whether key was found in trie
-void search(TrieNode *root, string key) {
-    int level = 0;
-    for (int i = 0; i < key.length(); i++) {
-        level++;
-        char index = key[i];
-        std::map<char, TrieNode*>::iterator it;
-        it = root->mymap.find(index);
+            if(this->root == NULL) {
+                this->root = new TrieNode();
+                TrieNode* temp = new TrieNode();
+                this->root->mymap['k'] = temp;
+                cout << this->root->isLeaf << endl;
+                if(this->root->mymap.size() == 1) {
+                    cout << "map has one key" << endl;
+                }
+            }
+            return;
 
-        if (it != root->mymap.end()) {
-            if (level < key.length())
-                root = root->mymap[index];
-        } else {
-            cout << "word not found: " << key << endl;
+
+//            int level = 0;
+//
+//            // tempRoot already has a map defined
+//            for(int i = 0; i < key.length(); i++) {
+//                level++;
+//                char index = key[i];
+//                unordered_map<char, TrieNode*>::iterator it;
+//                if(tempRoot == NULL) {
+//                    tempRoot = new TrieNode();
+//                }
+//                it = tempRoot->mymap.find(index);
+//
+//                //found char, move on to next char
+//                if(it != tempRoot->mymap.end()) {
+//                    tempRoot = tempRoot->mymap[index];
+//                // new character
+//                } else {
+//                    TrieNode* temp = new TrieNode();
+//                    tempRoot->mymap[index] = temp;
+//                    if (level < key.length()) {
+//                        tempRoot = tempRoot->mymap[index];
+//                    }
+//                }
+//            }
+//
+//            // mark last node as leaf
+//            tempRoot->isLeaf = true;
+        }
+
+        // Prints whether key was found in trie
+        void search(TrieNode *tempRoot, string key) {
+            int level = 0;
+
+            for (int i = 0; i < key.length(); i++) {
+                level++;
+                char index = key[i];
+                unordered_map<char, TrieNode*>::iterator it;
+                if(tempRoot == NULL) {
+                    cout << "Trie does not exist" << endl;
+                    return;
+                }
+                it = tempRoot->mymap.find(index);
+
+                if (it != tempRoot->mymap.end()) {
+                    if (level < key.length())
+                        tempRoot = tempRoot->mymap[index];
+                } else {
+                    cout << "word not found: " << key << endl;
+                    return;
+                }
+            }
+
+            if (tempRoot->isLeaf)
+                cout << "word found: " << key << endl;
+            else
+                cout << "word not found: " << key << endl;
+            
             return;
         }
-    }
 
-    if (root->isLeaf)
-        cout << "word found: " << key << endl;
-    else
-        cout << "word not found: " << key << endl;
-    
-    return;
-}
-
-bool delete_trie(TrieNode *root, string key, int level, int key_length) {
-    if (root == NULL) {
-        cout << "root is null" << endl;
-        return false;
-    }
-
-    // Base case
-    if( level == key_length - 1) {
-        //we are on the last letter of key
-        char index = key[level];
-        std::map<char, TrieNode*>::iterator it;
-        it = root->mymap.find(index);
-
-        if (it != root->mymap.end()) {
-            if (root->isLeaf) {
-                //make leaf false
-                root->isLeaf = false;
-
-                //if no children of last letter
-                if (root->mymap[index]->mymap.size() == 0) {
-                    //first delete the allocated map here so no memory leak
-                    delete root->mymap[index];
-
-                    //then remove element from map so that this map becomes and empty map in the end
-                    root->mymap.erase(index);
-                    return true;
-                } else {
-                    cout << "some more letters" << endl;
-                    return false;                
-                }
-            } else
+        bool delete_trie(TrieNode *root, string key, int level, int key_length) {
+            if (root == NULL) {
+                cout << "root is null" << endl;
                 return false;
-        } else
+            }
+
+            // Base case
+            if( level == key_length - 1) {
+                //we are on the last letter of key
+                char index = key[level];
+                unordered_map<char, TrieNode*>::iterator it;
+                it = root->mymap.find(index);
+
+                if (it != root->mymap.end()) {
+                    if (root->isLeaf) {
+                        //make leaf false
+                        root->isLeaf = false;
+
+                        //if no children of last letter
+                        if (root->mymap[index]->mymap.size() == 0) {
+                            //first delete the allocated map here so no memory leak
+                            delete root->mymap[index];
+
+                            //then remove element from map so that this map becomes and empty map in the end
+                            root->mymap.erase(index);
+                            return true;
+                        } else {
+                            cout << "some more letters" << endl;
+                            return false;                
+                        }
+                    } else
+                        return false;
+                } else
+                    return false;
+            }
+
+            char index = key[level];
+            unordered_map<char, TrieNode*>::iterator it;
+            it = root->mymap.find(index);
+
+            if (it != root->mymap.end()) {
+                root = root->mymap[index];
+                if (delete_trie(root, key, level+1, key_length)) {
+                    // check keys in upward manner
+                    cout << "in recursion: " << index << endl;
+                    if (flag == 1) {
+                        flag = 0;
+                        return true;
+                    }
+                    cout << root->mymap.size() << endl;
+
+                    //if no children of last letter
+                    if (root->mymap[index]->mymap.size() == 0) {
+                        //cout << "level: " << level << endl;
+                        //cout << "not more letters" << endl;
+                        //cout << root->mymap.size() << endl;
+                        root->mymap.erase(index);
+                        //cout << root->mymap.size() << endl;
+                        return true;
+                    } else {
+                        //cout << "some more letters" << endl;
+                        return false;                
+                    }
+                }
+                return false;
+            }
             return false;
-    }
-
-    char index = key[level];
-    std::map<char, TrieNode*>::iterator it;
-    it = root->mymap.find(index);
-
-    if (it != root->mymap.end()) {
-        root = root->mymap[index];
-        if (delete_trie(root, key, level+1, key_length)) {
-            // check keys in upward manner
-            cout << "in recursion: " << index << endl;
-            if (flag == 1) {
-                flag = 0;
-                return true;
-            }
-            cout << root->mymap.size() << endl;
-
-            //if no children of last letter
-            if (root->mymap[index]->mymap.size() == 0) {
-                //cout << "level: " << level << endl;
-                //cout << "not more letters" << endl;
-                //cout << root->mymap.size() << endl;
-                root->mymap.erase(index);
-                //cout << root->mymap.size() << endl;
-                return true;
-            } else {
-                //cout << "some more letters" << endl;
-                return false;                
-            }
         }
-        return false;
-    }
-    return false;
-}
+};
 
 int main() {
-    TrieNode* root = newTrieNode();
+    Trie trie;
 
     string keys[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
     int keys_size = sizeof(keys)/sizeof(keys[0]);
 
     // Construct trie
-    for(int i = 0; i < keys_size; i++) {
-        insert(root, keys[i]);
-    }
+    //for(int i = 0; i < keys_size; i++) {
+    //    TrieNode* temp = trie.root;
+    //    trie.insert(temp, keys[i]);
+    //}
+
+    trie.insert("the");
 
     cout << "------" << endl;
-    search(root, "the");
-    search(root, "these");
-    search(root, "their");
-    search(root, "thaw");
+    cout << trie.root->isLeaf << endl;
+    cout << trie.root->mymap.size() << endl;
+//    trie.search(temp, "the");
+//    trie.search(trie.root, "these");
+//    trie.search(trie.root, "their");
+//    trie.search(trie.root, "thaw");
     cout << "------" << endl;
-
-    delete_trie(root, keys[2], 0, keys[2].length());
-    delete_trie(root, keys[0], 0, keys[0].length());
-
-    cout << "======" << endl;
-    search(root, "there");
-    search(root, "their");
-    search(root, "the");
-    search(root, "ther");
-    cout << "======" << endl;
+//
+//    trie.delete_trie(trie.root, keys[2], 0, keys[2].length());
+//    trie.delete_trie(trie.root, keys[0], 0, keys[0].length());
+//     
+//    cout << "======" << endl;
+//    trie.search(trie.root, "there");
+//    trie.search(trie.root, "their");
+//    trie.search(trie.root, "the");
+//    trie.search(trie.root, "ther");
+//    cout << "======" << endl;
 
     return 0;
 }
