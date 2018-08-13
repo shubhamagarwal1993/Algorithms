f21b01501c17e3c6e95ba9c8f3eef960ea733384
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-13 09:11:44 -0500

    added binary search in matrix

--- graphs/matrix_search.cpp
+++ graphs/matrix_search.cpp
@@ -32,6 +32,38 @@ bool findElementInMatrix(int matrix[M][N], int num) {
     return false;
 }
 
+bool test(int matrix[M][N], int row_u, int row_d, int col_l, int col_r, int num, int& iter) {
+
+    if((row_d < row_u) || (col_r < col_l)) {
+        return false;
+    }
+
+    //if(iter > 10) {
+    //    return false;
+    //} else {
+    //    iter = iter + 1;
+    //}
+    int row_mid = (row_d + row_u)/2;
+    int col_mid = (col_r + col_l)/2;
+    int mid_value = matrix[row_mid][col_mid];
+
+    cout << row_u << " " << row_d << " " << col_l << " " << col_r << " " << mid_value << endl;
+    if(mid_value == num) {
+        return true;
+    } else if(mid_value > num) {
+        // num is in left half or top half
+        return test(matrix, row_u, row_d, col_l, col_mid - 1, num, iter)
+            || test(matrix, row_u, row_mid-1, col_l, col_r, num, iter);
+    } else if(mid_value < num) {
+//        cout << "inside greater case" << endl;
+        // num is in right half or bottom half
+        return test(matrix, row_u, row_d, col_mid+1, col_r, num, iter)
+            || test(matrix, row_mid + 1, row_d, col_l, col_r, num, iter);
+    } else {
+        return false;
+    }
+}
+
 int main() {
 
     int matrix[M][N] = {{10, 20, 30, 40, 50, 60}
@@ -40,20 +72,27 @@ int main() {
                        ,{32, 33, 39, 50, 59, 69}
                        };
 
-    for(int row = 0; row < M; row++) {
-        for(int col = 0; col < N; col++) {
-            cout << matrix[row][col] << " ";
-        }
-        cout << endl;
-    }
+//    for(int row = 0; row < M; row++) {
+//        for(int col = 0; col < N; col++) {
+//            cout << matrix[row][col] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    // find element in matrix
+    int element = 37;
+//    if(findElementInMatrix(matrix, element)) {
+//        cout << "Found element " << element << " in matrix" << endl;
+//    } else {
+//        cout << "Element not found" << endl;
+//    }
 
-    // find element in matrix
-    int element = 57;
-    if(findElementInMatrix(matrix, element)) {
-        cout << "Found element " << element << " in matrix" << endl;
-    } else {
-        cout << "Element not found" << endl;
-    }
+    int row_u = 0;
+    int row_d = M - 1;
+    int col_l = 0;
+    int col_r = N - 1;
+    int iter = 0;
+    cout << test(matrix, row_u, row_d, col_l, col_r, element, iter) << endl;
 
     return 0;
 }
