9ebd602084b256138880bf266d28ddd29382ff68
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-15 08:35:48 -0500

    completed counting islands in matrix

--- misc/number_of_islands.cpp
+++ misc/number_of_islands.cpp
@@ -1,67 +1,87 @@
 // Given a boolean 2D matrix, find the number of islands
 // A group of connected 1s forms an island
 
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-// function does DFS on given 1 to cover all elements in that island
-// Do a DFS on all 8 neighbours, as adjacent vertices
-void DFS(int M[][COL], int row, int col, bool visited[][COL]) {
+#define COL 5
+#define ROW 5
 
-	// These arrays are used to get row and column numbers of 8 neighbours of a given cell
-	static int rowNbr[] = {-1, -1, -1,  0, 0,  1, 1, 1};
-	static int colNbr[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+bool isSafe(int M[ROW][COL], int row, int col, bool visited[][COL]) {
+    if(row < 0 || row >= ROW) {
+        return false;
+    }
 
-	// Mark this cell as visited
-	visited[row][col] = true;
+    if(col < 0 || col >= COL) {
+        return false;
+    }
 
-	// Recur for all connected neighbours
-	for (int k = 0; k < 8; ++k) {
-		if(isSafe(M, row + rowNbr[k], col + colNbr[k], visited)) {
-			DFS(M, row + rowNbr[k], col + colNbr[k], visited);
-		}
-	}
+    if(visited[row][col]) {
+        return false;
+    }
+
+    if(!M[row][col]) {
+        return false;
+    }
+
+    return true;
 }
 
-// returns count of islands in a given boolean 2D matrix
-int countIslands(int M[][COL]) {
+void DFS(int M[ROW][COL], int row, int col, bool visited[ROW][COL]) {
 
-	// Make a bool array to mark visited cells, cells unvisited initially
-	bool visited[ROW][COL];
-	memset(visited, 0, sizeof(visited));
+    // These arrays are used to get row and column numbers of 8 neighbours of a given cell
+    static int rowNbr[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+    static int colNbr[] = {-1,  0,  1, -1, 1, -1, 0, 1};
 
-	int island_count = 0;
+    // Mark this cell as visited
+    visited[row][col] = true;
 
-	// visit all elements in 2D matrix
-	for(int i = 0; i < ROW; i++) {
-		for(int j = 0; j < COL; j++) {
-			// if cell has value 1 and is not visited, then new island found
-			if (M[i][j] && !visited[i][j]) {
-				// Take this element and find all connected 1s
-				// This will make all elements of current island visited
-				DFS(M, i, j, visited);
-				island_count += 1;
-			}
-		}
-	}
+    // Recur for all connected neighbours
+    for (int k = 0; k < 8; k++) {
+        if(isSafe(M, row + rowNbr[k], col + colNbr[k], visited)) {
+            DFS(M, row + rowNbr[k], col + colNbr[k], visited);
+        }
+    }
+}
 
-	return island_count;
+int countIslands(int M[ROW][COL]) {
+
+    // Make a bool array to mark visited cells, cells unvisited initially
+    bool visited[ROW][COL];
+    for(int i = 0; i < ROW; i++) {
+        for(int j = 0; j < COL; j++) {
+            visited[i][j] = 0;
+        }
+    }
+
+    int island_count = 0;
+
+    // visit all elements in 2D matrix
+    for(int i = 0; i < ROW; i++) {
+        for(int j = 0; j < COL; j++) {
+            // If cell has value 1 and is not visited, then new island found
+            if (M[i][j] && !visited[i][j]) {
+                // Take this element and find all connected 1s
+                // This will make all elements of current island visited
+                DFS(M, i, j, visited);
+                island_count += 1;
+            }
+        }
+    }
+
+    return island_count;
 }
 
 int main() {
-	int M[][COL] = {
-		{1, 1, 0, 0, 0},
-		{0, 1, 0, 0, 1},
-		{1, 0, 0, 1, 1},
-		{0, 0, 0, 0, 0},
-		{1, 0, 1, 0, 1}
-	};
+    int M[ROW][COL] = {{1, 1, 0, 0, 0},
+                       {0, 1, 0, 0, 1},
+                       {1, 0, 0, 1, 1},
+                       {0, 0, 0, 0, 0},
+                       {1, 0, 1, 0, 1}
+                      };
 
-	cout << "Number of islands are: " << countIslands(M) << endl;
+    cout << "Number of islands are: " << countIslands(M) << endl;
     return 0;
 }
+
