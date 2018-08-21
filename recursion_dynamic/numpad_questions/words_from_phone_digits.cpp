aa1c122ca10c338b3a463ebef2deb12f9ca86e05
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-21 10:09:28 -0500

    cleaned up code for finding words from given phone number

--- recursion_dynamic/numpad_questions/words_from_phone_digits.cpp
+++ recursion_dynamic/numpad_questions/words_from_phone_digits.cpp
@@ -4,39 +4,41 @@
 using namespace std;
 
 // 2d array to store phone numberpad digits to alphabets
-const string hashTable[10] = {"", "wsx", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-//                             0    1      2      3      4      5      6       7      8       9
+const string hashTable[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+//                             0   1    2      3      4      5      6       7      8       9
 
-void printWordsUtil(int number[], int curr_digit, char output[], int number_size, int &count) {
+void printWordsUtil(int num_array[], int num_array_size, int curr_digit, char result[]) {
+
     // Base case, if current output word is prepared
-    if(curr_digit == number_size) {
-        cout << output << endl;
-        count++;
+    if(curr_digit == num_array_size) {
+        for(int i = 0; i < num_array_size; i++) {
+            cout << result[i];
+        }
+        cout << endl;
         return;
     }
 
-    cout << hashTable[number[curr_digit]] << endl;
-    // Try all 3 possible characters for current digir in number[]
-    // and recur for remaining digits
-    for(int i = 0; i < (hashTable[number[curr_digit]]).length(); i++) {
-        output[curr_digit] = hashTable[number[curr_digit]][i];
-        printWordsUtil(number, curr_digit+1, output, number_size, count);
-        if(number[curr_digit] == 0 || number[curr_digit] == 1)
+    // Try all possible chars for current digit in num_array[] and recur for remaining digits
+    for(int i = 0; i < (hashTable[num_array[curr_digit]]).length(); i++) {
+        // add char to array and go to next num in array
+        result[curr_digit] = hashTable[num_array[curr_digit]][i];
+        printWordsUtil(num_array, num_array_size, curr_digit + 1, result);
+
+        if(num_array[curr_digit] == 0 || num_array[curr_digit] == 1) {
             return;
+        }
     }
 }
 
-void printWords(int number[], int number_size) {
-    char result[number_size + 1];
-    result[number_size] ='\0';
-    int count = 0;
-    printWordsUtil(number, 0, result, number_size, count);
-    cout << count << endl;
+void printWords(int num_array[], int num_array_size) {
+    char result[num_array_size];
+    int starting_index = 0;
+    printWordsUtil(num_array, num_array_size, starting_index, result);
 }
 
 int main() {
-    int number[] = {2, 7};//, 3, 4};
-    int size = sizeof(number)/sizeof(number[0]);
-    printWords(number, size);
+    int num_array[] = {2, 7};//, 3, 4};
+    int num_array_size = sizeof(num_array)/sizeof(num_array[0]);
+    printWords(num_array, num_array_size);
     return 0;
 }
