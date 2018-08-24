1654eb3eb40c4813cd4145a72da392213f6d71c2
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-24 09:35:47 -0500

    removed executable files

--- recursion_dynamic/contiguous subarray/largest_sum_contiguous_subarray.cpp
+++ recursion_dynamic/contiguous subarray/largest_sum_contiguous_subarray.cpp
@@ -8,23 +8,36 @@ using namespace std;
 int maxSubArraySum(int arr[], int arr_size) {
 	// will maintain max value at any point of time
 	int max_so_far = 0;
+    int max_so_far_index = 0;
+    int max_length_so_far = 0;
+
+    int max_index_final = 0;
+    int max_length_final = 0;
 	// will check sum on each iteration
 	int max_ending_here = 0;
 
 	for(int i = 0; i < arr_size; i++) {
 		// add current value to check if will be kept or removed
 		max_ending_here = max_ending_here + arr[i];
+        max_length_so_far++;
 
 		// update max value
 		if(max_so_far < max_ending_here) {
 			max_so_far = max_ending_here;
+            max_length_final = max_length_so_far;
+            max_index_final = max_so_far_index;
 		}
 
 		// if less than 0, then dont add
 		if(max_ending_here < 0) {
 			max_ending_here = 0;
+            max_so_far_index = i + 1;
+            max_length_so_far = 0;
 		}
 	}
+
+    cout << max_index_final << endl;
+    cout << max_length_so_far << endl;
 	return max_so_far;
 }
 
