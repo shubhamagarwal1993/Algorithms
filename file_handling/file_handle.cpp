20766283804e1b4c7fa2a8eb587747ebfea48323
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-08-26 18:33:15 -0500

    added total lines in file code

--- file_handling/file_handle.cpp
+++ file_handling/file_handle.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
     string line;
-    ifstream input_file ("input.txt");
+    ifstream input_file("input.txt");
 
     if(input_file.is_open()) {
+
+        // total lines in file
+        int total_lines = count(istreambuf_iterator<char>(input_file), istreambuf_iterator<char>(), '\n');
+        cout << "Total lines in file: " << total_lines << endl;
 
         // create a new file
         ofstream output_file;
@@ -16,6 +22,29 @@ int main() {
 
         while(getline(input_file, line)) {
             output_file << line << endl;
+
+            // count for 3 dots in string
+            size_t ip_addr_break_points = count(line.begin(), line.end(), '.');
+            if(ip_addr_break_points != 3) {
+                // Do something
+            }
+
+            // split ip addr segments into vector
+            stringstream ip_addr_ss(line);
+            string ip_addr_segment;
+            vector<string> ip_addr_vec;
+            while(getline(ip_addr_ss, ip_addr_segment, '.')) {
+                ip_addr_vec.push_back(ip_addr_segment);
+            }
+            if(ip_addr_vec.size() != 4) {
+                // Do something
+            }
+
+            // loop over each segment
+            for(int i = 0; i < ip_addr_vec.size(); i++) {
+                ip_addr_segment = ip_addr_vec[i];
+                    // Do something
+            }
         }
 
         input_file.close();
