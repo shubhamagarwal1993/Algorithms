f76dde8eb423ff85a8a4c28b91c9a6f9a4dcb781
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2018-09-18 16:28:29 -0500

    comparing double values

--- recursion_dynamic/8.11_cents_pennies.cpp
+++ recursion_dynamic/8.11_cents_pennies.cpp
@@ -72,6 +72,37 @@ int countWays_recursive(int denominations[], int den_size, int total, vector<int
     return with + without;
 }
 
+bool doubleEquals(double a, double b, double epsilon = 0.001) {
+    return std::abs(a - b) < epsilon;
+}
+
+int countWaysRecursive(double denominations[], int den_size, double total, vector<double> vec, double finalVal) {
+
+    if(doubleEquals(total, finalVal)) {
+        for(int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+        return 1;
+    }
+
+    if(total < 0) {
+        return 0;
+    }
+
+    if(den_size == 0 && total > 0) {
+        return 0;
+    }
+
+    int without = countWaysRecursive(denominations, den_size - 1, total, vec, finalVal);
+
+    vec.push_back(denominations[den_size - 1]);
+    int with = countWaysRecursive(denominations, den_size, total - denominations[den_size - 1], vec, finalVal);
+    vec.pop_back();
+
+    return with + without;
+}
+
 int main() {
 
     // total value
@@ -90,6 +121,16 @@ int main() {
     cout << "Total combinations recursively: " << endl;
     int total_ways = countWays_recursive(denominations, 4, total, vec);
     cout << "total combinations = " << total_ways << endl;
+
+    double arr[] = {5.80, 4.20, 3.55, 3.35, 2.75, 2.15};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    //double sum = 4.90;
+    double sum = 4.30;
+    vector<double> vecDouble;
+    double finalVal = 0;
+    cout << "Double ways" << endl;
+    countWaysRecursive(arr, arr_size, sum, vecDouble, finalVal);
+
     return 0;
 }
 
