511f4de4976081012ab2d9cf0a1322d8f20eb243
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2020-01-08 16:36:54 -0800

    updated 2nd largest

--- trees/2nd_largest_element_BST.cpp
+++ trees/2nd_largest_element_BST.cpp
@@ -13,6 +13,7 @@ class Node {
         Node* left;
         Node* right;
 
+        // constructor
         Node(int data) {
             this->data = data;
             this->left = NULL;
@@ -24,12 +25,24 @@ class Tree {
 
     public:
         Node* root;
+
+        // constructor
         Tree() {
             this->root = NULL;
         }
 
-        Node* construct_tree() {
+        void construct_tree() {
 
+            //        50
+            //        / \
+            //       /   \
+            //      /     \
+            //     /       \
+            //    10       60
+            //   /  \     /  \
+            //  5   20  55   70
+            //          /   /  \
+            //        51   65  80
             Node* root = new Node(50);
             root->left = new Node (40);
             root->left->right = new Node (45);
@@ -52,91 +65,88 @@ class Tree {
             //temp->right->left = new Node(65);
             //temp->right->right = new Node(80);
 
-            return root;
+            this->root = root;
+            return;
+        }
+
+        int get2ndLargestInBSTInorder() {
+            int node_count = 0;
+            return this->get2ndLargestInBSTInorderUtil(this->root, node_count);
+        }
+
+        void get2ndLargestInBST() {
+            // a temp variable, so that the original root value does not change
+            Node* root = this->root;
+
+            // root cannot be null and must have at least 1 child
+            if((root == NULL) || (root->right == NULL && root->left == NULL)) {
+                cout << "Cannot find the 2nd largest element" << endl;
+                return;
+            }
+
+            // if tree does not have a right subtree, then largest in left subtree
+            if(root->left && (root->right == NULL)) {
+                Node* secondLargest = this->getRightMostNode(root->left);
+                cout << "2nd largest element = " << secondLargest->data << endl;
+                return;
+            }
+
+            while(root) {
+                // if current is the parent of the largest element, and the largest has no children
+                if(root->right && (root->right->left == NULL) && (root->right->right == NULL)) {
+                    cout << "2nd largest element = " << root->data << endl;
+                    return;
+                }
+
+                root = root->right;
+            }
+
+            cout << "1 Cannot find the 2nd largest element" << endl;
+            return;
+        }
+
+    private:
+        // helper function for get2ndLargestInBSTInorder
+        int get2ndLargestInBSTInorderUtil(Node* root, int node_count) {
+            if(root == NULL) {
+                return node_count;
+            }
+
+            node_count = get2ndLargestInBSTInorderUtil(root->right, node_count);
+
+            node_count++;
+            if(node_count == 2) {
+                cout << "2nd largest element = " << root->data << endl;
+            }
+
+            node_count = get2ndLargestInBSTInorderUtil(root->left, node_count);
+            return node_count;
+        }
+
+        Node* getRightMostNode(Node* root) {
+            if(root == NULL) {
+                return NULL;
+            }
+
+            if(root->right == NULL) {
+                return root;
+            }
+            return getRightMostNode(root->right);
         }
 };
-
-//        50
-//        / \
-//       /   \
-//      /     \
-//     /       \
-//    10       60
-//   /  \     /  \
-//  5   20  55   70
-//          /   /  \
-//        51   65  80
-
-
-Node* getRightMostNode(Node* root) {
-    if(root == NULL) {
-        return NULL;
-    }
-
-    if(root->right == NULL) {
-        return root;
-    }
-    return getRightMostNode(root->right);
-}
-
-Node* get2ndLargestInBST(Node* root) {
-    // root cannot be null and must have at least 1 child
-    if((root == NULL) || (root->right == NULL && root->left == NULL)) {
-        return NULL;
-    }
-
-    // if tree does not have a right subtree, then largest in left subtree
-    if(root->left && (root->right == NULL)) {
-        return getRightMostNode(root->left);
-    }
-
-    Node* current = root;
-    while(current) {
-
-        // if current is the parent of the largest element, and the largest has no children
-        if(current->right && (current->right->left == NULL) && (current->right->right == NULL)) {
-            return current;
-        }
-
-        current = current->right;
-    }
-    return NULL;
-}
-
-int get2ndLargestInBSTInorder(Node* root, int node_count) {
-    if(root == NULL) {
-        return node_count;
-    }
-
-    node_count = get2ndLargestInBSTInorder(root->right, node_count);
-
-    node_count++;
-    if(node_count == 2) {
-        cout << "2nd largest element = " << root->data << endl;
-    }
-
-    node_count = get2ndLargestInBSTInorder(root->left, node_count);
-    return node_count;
-}
 
 int main() {
 
     Tree tree;
-    tree.root = tree.construct_tree();
+    tree.construct_tree();
 
-    // By using inorder method - 2nd node in reverse inorder
+    // Method1: Get inorder, and 2nd node in reverse inorder
     // Running time O(n), Space O(1)
-    int node_count = 0;
-    get2ndLargestInBSTInorder(tree.root, node_count);
+    tree.get2ndLargestInBSTInorder();
 
-    // By finding largest node and then second largest
+    // Method2: Find the largest node, and then the second largest
     // Running time O(n), Space O(1)
-    Node* secondLargest = get2ndLargestInBST(tree.root);
-    if(secondLargest == NULL) {
-        cout << "Cannot find the 2nd largest element" << endl;
-    } else {
-        cout << "2nd largest element = " << secondLargest->data << endl;
-    }
+    tree.get2ndLargestInBST();
 
     return 0;
 }
