a8340875c77dcbd91e6f89081f4a67c977678644
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2020-01-11 09:35:48 -0800

    tree from inorder and preorder

--- trees/serialize_deserialize.cpp
+++ trees/serialize_deserialize.cpp
@@ -1,203 +1,213 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-Node* newNode(int data) {
-    Node* root = new Node();
-    root->data = data;
-    root->left = NULL;
-    root->right = NULL;
-    return root;
-}
+class Tree {
+    public:
+        Node* root;
 
-Node* constructTree() {
-    Node* root          = newNode(1);
-    root->left          = newNode(2);
-    root->right         = newNode(3);
-    root->right->left   = newNode(4);
-    root->right->right  = newNode(5);
-    return root;
-}
-
-void getInOrder(Node* root, string &inOrder) {
-    if(root == NULL) {
-        return;
-    }
-
-    getInOrder(root->left, inOrder);
-    if(!inOrder.empty()) {
-        inOrder = inOrder + "," + to_string(root->data);
-    } else {
-        inOrder = to_string(root->data);
-    }
-    getInOrder(root->right, inOrder);
-}
-
-void getPreOrder(Node* root, string &preOrder) {
-    if(root == NULL) {
-        return;
-    }
-
-    if(!preOrder.empty()) {
-        preOrder = preOrder + "," + to_string(root->data);
-    } else {
-        preOrder = to_string(root->data);
-    }
-    getPreOrder(root->left, preOrder);
-    getPreOrder(root->right, preOrder);
-}
-
-string serialize(Node* root) {
-    string inOrder = "";
-    getInOrder(root, inOrder);
-
-    string preOrder = "";
-    getPreOrder(root, preOrder);
-
-    return inOrder + "," + preOrder;;
-}
-
-Node* getBackTree(int inOrder[], int inOrder_l, int inOrder_r, int preOrder[], int &preOrder_index) {
-
-    if(inOrder_l > inOrder_r) {
-        return NULL;
-    }
-
-    int data = preOrder[preOrder_index];
-    Node* root = newNode(data);
-    preOrder_index++;
-
-    // find index from inorder
-    int k = -1;
-    for(int i = inOrder_l; i <= inOrder_r; i++) {
-        if(root->data == inOrder[i]) {
-            k = i;
-            break;
+        Tree() {
+            this->root = NULL;
         }
-    }
 
-    root->left  = getBackTree(inOrder, inOrder_l, k - 1, preOrder, preOrder_index);
-    root->right = getBackTree(inOrder, k + 1, inOrder_r, preOrder, preOrder_index);
-    return root;
-}
+        void constructTree() {
 
-int height(Node* node) {
-    if ( node == NULL) {
-        return 0;
-    } else {
-        // compute the height of each subtree
-        int lheight = height(node->left);
-        int rheight = height(node->right);
-
-        // use the larger one
-        if (lheight > rheight) {
-            return(lheight+1);
-        } else {
-            return(rheight+1);
+            Node* root          = new Node(1);
+            root->left          = new Node(2);
+            root->right         = new Node(3);
+            root->right->left   = new Node(4);
+            root->right->right  = new Node(5);
+            this->root = root;
+            return;
         }
-    }
-}
 
-// Print nodes at a given level
-void printGivenLevel(Node* root, int level) {
-    if (root == NULL) {
-        cout << "X" << " ";
-        return;
-    }
+        // inorder as a string
+        string getInorderString() {
+            if(this->root == NULL) {
+                return "";
+            }
 
-    if (level == 1) {
-        cout << root->data << " ";
-    } else if (level > 1) {
-        printGivenLevel(root->left, level-1);
-        printGivenLevel(root->right, level-1);
-    }
-}
-
-//print tree to check
-void printLevelOrder(Node* root) {
-    int h = height(root);
-    for (int i = 1; i <= h; i++) {
-        printGivenLevel(root, i);
-        cout << endl;
-    }
-}
-
-int getNodeCount(string serializedTree) {
-
-    int len = 0;
-    for(int i = 0; i < serializedTree.length(); i++) {
-        if(serializedTree[i] == ',') {
-            len = len + 1;
+            Node* root = this->root;
+            return getInorderStringUtil(root);
         }
-    }
 
-    return (len == 0) ? len : len + 1;
-}
+        // preorder as a string
+        string getPreorderString() {
+            if(this->root == NULL) {
+                return "";
+            }
 
-void deserialize(string serializedTree) {
+            Node* root = this->root;
+            string preorder_string = getPreorderStringUtil(root);
 
-    int treeLength = getNodeCount(serializedTree);
+            // Remove last comma if it exists
+            string preorder_string_formatted = "";
+            int preorder_string_size = preorder_string.size();
+            if(preorder_string[preorder_string_size - 1] == ',') {
+                preorder_string_formatted = preorder_string.substr(0, preorder_string.size()-1);
+            } else {
+                preorder_string_formatted = preorder_string;
+            }
 
-    int* inOrder = new int[treeLength/2];
-    int inOrder_index = 0;
-    int* preOrder = new int[treeLength/2];
-    int preOrder_index = 0;
-
-    string word;
-    int st_index = 0;
-    cout << serializedTree << endl;
-    while(inOrder_index < treeLength/2) {
-        if(serializedTree[st_index] == ',') {
-            inOrder[inOrder_index] = stoi(word);
-            word = "";
-            inOrder_index++;
-        } else if(st_index == serializedTree.length()) {
-            inOrder[inOrder_index] = stoi(word);
-            word = "";
-            inOrder_index++;
-        } else {
-            word = word + serializedTree[st_index];
+            return preorder_string_formatted;
         }
-        st_index++;
-    }
 
-    while(preOrder_index < treeLength/2) {
-        if(serializedTree[st_index] == ',') {
-            preOrder[preOrder_index] = stoi(word);
-            word = "";
-            preOrder_index++;
-        } else if(st_index + 1 == serializedTree.length()) {
-            word = word + serializedTree[st_index];
-            preOrder[preOrder_index] = stoi(word);
-            word = "";
-            preOrder_index++;
-        } else {
-            word = word + serializedTree[st_index];
+        void preorder() {
+            if(this->root == NULL) {
+                return;
+            }
+
+            Node* root = this->root;
+            preorderUtil(root);
+            cout << endl;
+            return;
         }
-        st_index++;
-    }
 
-    preOrder_index = 0;
-    Node* root = getBackTree(inOrder, 0, treeLength/2 - 1, preOrder, preOrder_index);
-    cout << "=======================" << endl;
-    printLevelOrder(root);
-    cout << "=======================" << endl;
-    return;
-}
+        string getSerializedTree() {
+            if(this->root == NULL) {
+                return "";
+            }
+
+            string inorder = getInorderString();
+            string preorder = getPreorderString();
+            if(inorder.empty() || preorder.empty()) {
+                return "";
+            }
+
+            return inorder + "," + preorder;
+        }
+
+        void deserialize(string serializedTree) {
+            if(serializedTree.empty()) {
+                return;
+            }
+
+            // Get inorder string
+            vector<int> inorder;
+            for(int i = 0; i < serializedTree.length()/2; i++) {
+                if(serializedTree[i] != ',') {
+                    inorder.push_back(serializedTree[i] - '0');
+                }
+            }
+            int inorder_start = 0;
+            int inorder_end = inorder.size() - 1;
+
+            // Get preorder string
+            vector<int> preorder;
+            for(int i = serializedTree.length()/2; i < serializedTree.length(); i++) {
+                if(serializedTree[i] != ',') {
+                    preorder.push_back(serializedTree[i] - '0');
+                }
+            }
+            int preorder_idx = 0;
+
+            this->root = deserializeUtil(inorder, preorder, inorder_start, inorder_end, preorder_idx);
+            return;
+        }
+
+    private:
+        string getInorderStringUtil(Node* root) {
+            if(root == NULL) {
+                return "";
+            }
+
+            string left = "";
+            if(root->left) {
+                left = getInorderStringUtil(root->left);
+            }
+
+            string res = to_string(root->data);
+
+            string right = "";
+            if(root->right) {
+                right = getInorderStringUtil(root->right);
+            }
+
+            return (left.empty() ? "" : left + ",") + res + (right.empty() ? "" : "," + right);
+        }
+
+        string getPreorderStringUtil(Node* root) {
+            if(root == NULL) {
+                return "";
+            }
+
+            string res = to_string(root->data);
+
+            string left = "";
+            if(root->left) {
+                left = getPreorderStringUtil(root->left);
+            }
+
+            string right = "";
+            if(root->right) {
+                right = getPreorderStringUtil(root->right);
+            }
+
+            return res + "," + (left.empty() ? "" : left) + (right.empty() ? "" : right);
+        }
+
+        Node* deserializeUtil(vector<int>inorder, vector<int>preorder, int inorder_start, int inorder_end, int &preorder_idx) {
+            if(inorder_start > inorder_end) {
+                return NULL;
+            }
+
+            Node* root = new Node(preorder[preorder_idx]);
+            preorder_idx += 1;
+
+            int inorder_idx = findIdx(inorder, root->data);
+            if(inorder_idx == -1) {
+                return NULL;
+            }
+
+            root->left = deserializeUtil(inorder, preorder, inorder_start, inorder_idx - 1, preorder_idx);
+            root->right = deserializeUtil(inorder, preorder, inorder_idx + 1, inorder_end, preorder_idx);
+            return root;
+        }
+
+        int findIdx(vector<int> vec, int data) {
+            for(int i = 0; i < vec.size(); i++) {
+                if(vec[i] == data) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        void preorderUtil(Node* root) {
+            if(root == NULL) {
+                return;
+            }
+
+            cout << root->data << " ";
+            preorderUtil(root->left);
+            preorderUtil(root->right);
+        }
+};
 
 int main() {
 
-    Node* root = constructTree();
+    Tree tree;
+    tree.constructTree();
 
-    string serializedTree = serialize(root);
-    deserialize(serializedTree);
+    string serialized_tree = tree.getSerializedTree();
+    tree.deserialize(serialized_tree);
+    tree.preorder();
     return 0;
 }
