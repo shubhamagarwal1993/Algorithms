83d56d870f7c26f8ed1b29d654c215bdba8d95fb
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2020-02-07 00:09:20 -0800

    knapsick with memoization

--- computational_complexity/knapsack_problem.cpp
+++ computational_complexity/knapsack_problem.cpp
@@ -9,6 +9,83 @@
 
 using namespace std;
 
+void knapsack_memoization() {
+    // init
+    int items_weight[] = {5, 4, 6, 3};
+    int items_value[] = {10, 40, 30, 50};
+    int items_size = 4;
+    int weight_limit = 10;
+
+    // logic
+    int mat[weight_limit + 1];
+    for(int i = 0; i < weight_limit + 1; i++) {
+        mat[i] = 0;
+    }
+
+    for(int item_idx = 1; item_idx < items_size + 1; item_idx++) {
+        for(int weight = weight_limit; weight >= 1; weight--) {
+            // At row i and column j, we are tackling a sub-problem consisting of items 1, 2, 3 … i with a knapsack of j capacity.
+            // There are 2 options at this point: we can either include item i or not.
+            int without_value = mat[weight];
+
+            int with_value = 0;
+            int curr_weight = items_weight[item_idx - 1];
+            if(weight >= curr_weight) {
+                with_value = items_value[item_idx - 1];
+
+                int weight_remaining = weight - curr_weight;
+                with_value += mat[weight_remaining];
+            }
+
+            mat[weight] = max(without_value, with_value);
+        }
+    }
+
+    cout << mat[weight_limit] << endl;
+}
+
+void knapsack_dynamic() {
+    // init
+    int items_weight[] = {5, 4, 6, 3};
+    int items_value[] = {10, 40, 30, 50};
+    int items_size = 4;
+    int weight_limit = 10;
+
+    // logic
+    int mat[items_size+1][weight_limit+1];
+
+    // row 0 has no items to pick from
+    for(int col = 0; col < weight_limit + 1; col++) {
+        mat[0][col] = 0;
+    }
+
+    // col 0 has no weight to allow any items
+    for(int row = 0; row < items_size + 1; row++) {
+        mat[row][0] = 0;
+    }
+
+    for(int item_idx = 1; item_idx < items_size + 1; item_idx++) {
+        for(int weight = 1; weight < weight_limit + 1; weight++) {
+            // At row i and column j, we are tackling a sub-problem consisting of items 1, 2, 3 … i with a knapsack of j capacity.
+            // There are 2 options at this point: we can either include item i or not.
+            int without_value = mat[item_idx-1][weight];
+
+            int with_value = 0;
+            int curr_weight = items_weight[item_idx - 1];
+            if(weight >= curr_weight) {
+                with_value = items_value[item_idx - 1];
+
+                int weight_remaining = weight - curr_weight;
+                with_value += mat[item_idx - 1][weight_remaining];
+            }
+
+            mat[item_idx][weight] = max(without_value, with_value);
+        }
+    }
+
+    cout << mat[items_size][weight_limit] << endl;
+}
+
 bool knapsack_naive_util(int items_weight[], int items_value[], int items_size, int value_curr, int &max_value, int weight_curr, int weight_limit, vector<int> items_vec, vector<int> &items_final) {
 
     // end of combination
@@ -66,5 +143,17 @@ void knapsack_naive() {
 int main() {
     // Exponential soln. Looks for all possible combinations and picks one with greatest value
     knapsack_naive();
+
+    // N: size of items
+    // M: weight limit
+    // Space complexity: O(N*M)
+    // Time complexity: O(N*M) since 2d array is filled once.
+    knapsack_dynamic();
+
+    // N: size of items
+    // M: weight limit
+    // Space complexity: O(M). N is no longer required since we can maintain 2 rows and get the results.
+    // Time complexity: O(N*M) since all rows and col are visited once, or twice max when looking for max value in a later computation.
+    knapsack_memoization();
     return 0;
 }
